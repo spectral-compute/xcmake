4a705583c9a45446d3d6c1adff6a39f09c5bc3e0
Chris Kitching <chriskitching@linux.com>
2017-10-12 05:43:02 +0100

    A routine for pruning cmake argument lists

---
+++ scripts/ArgHandle.cmake
@@ -0,0 +1,41 @@
+# Routines for manipulating cmake argument lists.
+# A common problem is a wish to pass argument lists - mutated - to some other function. This is
+# surprisingly fiddly to do.
+
+macro (remove_argument TYPE LIST NAME)
+    # Remove the argument of the given name and type from the argument list named by "${LIST}".
+    # TYPE can be "FLAG", "SINGLE", or "MULTI", and has meaning equivalent to that in
+    # cmake_parse_arguments.
+    # In the "MULTI" case, you must pass all other keys as an additional argument.
+
+    # Find the key.
+    list(FIND ${LIST} ${NAME} KEY_INDEX)
+
+    # Remove it, if found. Otherwise stop.
+    if (NOT ${KEY_INDEX} LESS 0)
+        list(REMOVE_AT ${LIST} ${KEY_INDEX})
+
+        if (TYPE STREQUAL "FLAG")
+            # A flag is only a key - so we're done.
+        elseif(TYPE STREQUAL "SINGLE")
+            # Remove the value - which must exist if the argument list is well-formed.
+            list(REMOVE_AT ${LIST} ${KEY_INDEX})
+        elseif(TYPE STREQUAL "MULTI")
+            # Keep removing values until we find something that's a key, or outside the list.
+            list(LENGTH ${LIST} LIST_LENGTH)
+            while(LIST_LENGTH GREATER KEY_INDEX)
+                list(GET ${LIST} ${KEY_INDEX} NEXT_DELETION)
+                list(FIND ARGN ${NEXT_DELETION} SENTINEL)
+                if (SENTINEL LESS 0)
+                    break()
+                endif()
+
+                list(REMOVE_AT ${LIST} ${KEY_INDEX})
+                list(LENGTH ${LIST} LIST_LENGTH)
+            endwhile()
+
+        else()
+            message(FATAL_ERROR "Invalid argument type: ${TYPE}")
+        endif()
+    endif ()
+endmacro()
