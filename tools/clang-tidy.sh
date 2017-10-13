bcc20bb24e85320b4f962b4144b8a25547dcdb11
Chris Kitching <chriskitching@linux.com>
2017-10-13 06:30:11 +0100

    Fix bash syntax errors in clang-tidy.sh... :S

--- tools/clang-tidy.sh
+++ tools/clang-tidy.sh
@@ -6,101 +6,105 @@ printError() {
 
 # Function for detecting some kinds of trivial formatting fail.
 findFail() {
-    FAIL=`grep -n $1 $2 | head -n 1 | cut -d ':' -f 1`
+    REGEX=$1
+    FILE=$2
+    shift 2
+
+    FAIL=`grep -Pn $REGEX $FILE | head -n 1 | cut -d ':' -f 1`
     if [ ! $FAIL = "" ]; then
-        printError $2:$FAIL $3
+        printError $FILE:$FAIL "$@"
         exit 1
     fi
 }
 
 # Weed out trivial formatting fails right away...
-findFail $'^ *\t' $1 "line indented with tab characters"
+findFail '\t' $1 "line indented with tab characters"
 findFail $'\\s$' $1 "Trailing whitespace"
 findFail $'\r'\$ $1 "CRLF detected - use LF instead"
 
 # Most, but not quite all, of the available checks:
 # https://clang.llvm.org/extra/clang-tidy/checks/list.html
 CHECKS=clang-analyzer-*,\
-       boost-use-to-string,\
-       bugprone-integer-division,\
-       bugprone-suspicious-memset-usage,\
-       bugprone-undefined-memory-manipulation,\
-       cert-err52-cpp,\
-       cert-err60-cpp,\
-       cert-flp30-c,\
-       cppcoreguidelines-interfaces-global-init,\
-       cppcoreguidelines-no-malloc,\
-       cppcoreguidelines-slicing,\
-       google-explicit-constructor,\
-       google-runtime-member-string-references,\
-       llvm-namespace-comment,\
-       misc-assert-side-effect,\
-       misc-bool-pointer-implicit-conversion,\
-       misc-dangling-handle,\
-       misc-definitions-in-headers,\
-       misc-forward-declaration-namespace,\
-       misc-inaccurate-erase,\
-       misc-incorrect-roundings,\
-       misc-inefficient-algorithm,\
-       misc-lambda-function-name,\
-       misc-move-constructor-init,\
-       misc-move-forwarding-reference,\
-       misc-multiple-statement-macro,\
-       misc-non-copyable-objects,\
-       misc-redundant-expression,\
-       misc-string-constructor,\
-       misc-string-integer-assignment,\
-       misc-string-literal-with-embedded-nul,\
-       misc-suspicious-enum-usage,\
-       misc-suspicious-missing-comma,\
-       misc-suspicious-semicolon,\
-       misc-swapped-arguments,\
-       misc-unconventional-assign-operator,\
-       misc-undelegated-constructor,\
-       misc-uniqueptr-reset-release,\
-       misc-unused-alias-decls,\
-       misc-unused-raii,\
-       misc-unused-using-decls,\
-       misc-use-after-move,\
-       modernize-deprecated-headers,\
-       modernize-loop-convert,\
-       modernize-make-unique,modernize-make-shared,\
-       modernize-redundant-void-arg,\
-       modernize-replace-auto-ptr,\
-       modernize-replace-random-shuffle,\
-       modernize-shrink-to-fit,\
-       modernize-use-bool-literals,\
-       modernize-use-emplace,\
-       modernize-use-equals-default,modernize-use-equals-delete,\
-       modernize-use-noexcept,\
-       modernize-use-nullptr,\
-       modernize-use-override,\
-       performance-faster-string-find,\
-       performance-for-range-copy,\
-       performance-implicit-conversion-in-loop,\
-       performance-inefficient-string-concatenation,\
-       performance-inefficient-vector-operation,\
-       performance-type-promotion-in-math-fn,\
-       performance-unnecessary-copy-initialization,\
-       performance-unnecessary-value-param,\
-       readability-avoid-const-params-in-decls,\
-       readability-braces-around-statements,\
-       readability-container-size-empty,\
-       readability-deleted-default,\
-       readability-delete-null-pointer,\
-       readability-misleading-indentation,\
-       readability-misplaced-array-index,\
-       readability-redundant-control-flow,\
-       readability-redundant-declaration,\
-       readability-redundant-function-ptr-dereference,\
-       readability-redundant-member-init,\
-       readability-redundant-smartptr-get,\
-       readability-redundant-string-cstr,\
-       readability-redundant-string-init,\
-       readability-simplify-boolean-expr,\
-       readability-static-accessed-through-instance,\
-       readability-static-definition-in-anonymous-namespace,\
-       readability-uniqueptr-delete-release
+boost-use-to-string,\
+bugprone-integer-division,\
+bugprone-suspicious-memset-usage,\
+bugprone-undefined-memory-manipulation,\
+cert-err52-cpp,\
+cert-err60-cpp,\
+cert-flp30-c,\
+cppcoreguidelines-interfaces-global-init,\
+cppcoreguidelines-no-malloc,\
+cppcoreguidelines-slicing,\
+google-explicit-constructor,\
+google-runtime-member-string-references,\
+llvm-namespace-comment,\
+misc-assert-side-effect,\
+misc-bool-pointer-implicit-conversion,\
+misc-dangling-handle,\
+misc-definitions-in-headers,\
+misc-forward-declaration-namespace,\
+misc-inaccurate-erase,\
+misc-incorrect-roundings,\
+misc-inefficient-algorithm,\
+misc-lambda-function-name,\
+misc-move-constructor-init,\
+misc-move-forwarding-reference,\
+misc-multiple-statement-macro,\
+misc-non-copyable-objects,\
+misc-redundant-expression,\
+misc-string-constructor,\
+misc-string-integer-assignment,\
+misc-string-literal-with-embedded-nul,\
+misc-suspicious-enum-usage,\
+misc-suspicious-missing-comma,\
+misc-suspicious-semicolon,\
+misc-swapped-arguments,\
+misc-unconventional-assign-operator,\
+misc-undelegated-constructor,\
+misc-uniqueptr-reset-release,\
+misc-unused-alias-decls,\
+misc-unused-raii,\
+misc-unused-using-decls,\
+misc-use-after-move,\
+modernize-deprecated-headers,\
+modernize-loop-convert,\
+modernize-make-unique,modernize-make-shared,\
+modernize-redundant-void-arg,\
+modernize-replace-auto-ptr,\
+modernize-replace-random-shuffle,\
+modernize-shrink-to-fit,\
+modernize-use-bool-literals,\
+modernize-use-emplace,\
+modernize-use-equals-default,modernize-use-equals-delete,\
+modernize-use-noexcept,\
+modernize-use-nullptr,\
+modernize-use-override,\
+performance-faster-string-find,\
+performance-for-range-copy,\
+performance-implicit-conversion-in-loop,\
+performance-inefficient-string-concatenation,\
+performance-inefficient-vector-operation,\
+performance-type-promotion-in-math-fn,\
+performance-unnecessary-copy-initialization,\
+performance-unnecessary-value-param,\
+readability-avoid-const-params-in-decls,\
+readability-braces-around-statements,\
+readability-container-size-empty,\
+readability-deleted-default,\
+readability-delete-null-pointer,\
+readability-misleading-indentation,\
+readability-misplaced-array-index,\
+readability-redundant-control-flow,\
+readability-redundant-declaration,\
+readability-redundant-function-ptr-dereference,\
+readability-redundant-member-init,\
+readability-redundant-smartptr-get,\
+readability-redundant-string-cstr,\
+readability-redundant-string-init,\
+readability-simplify-boolean-expr,\
+readability-static-accessed-through-instance,\
+readability-static-definition-in-anonymous-namespace,\
+readability-uniqueptr-delete-release
 
 
 TIDY_ARGS="-warnings-as-errors=* -checks=$CHECKS $@"
