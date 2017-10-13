d279dfc72878fe160928a2e79f5da39b2d1d9188
Chris Kitching <chriskitching@linux.com>
2017-10-13 06:31:08 +0100

    Add global CLANG_TIDY property

---
+++ scripts/properties/CLANG_TIDY.cmake
@@ -0,0 +1,12 @@
+define_xcmake_global_property(
+    CLANG_TIDY FLAG
+    DEFAULT OFF
+)
+
+function(CLANG_TIDY_EFFECTS TARGET)
+    set_target_properties(
+        ${TARGET}
+        PROPERTIES CXX_CLANG_TIDY ${XCMAKE_TOOLS_DIR}/clang-tidy.sh
+    )
+    message(${XCMAKE_TOOLS_DIR}/clang-tidy.sh)
+endfunction()
