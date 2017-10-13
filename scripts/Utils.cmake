31c70c5e42cf63de5dbca4a61a73895c925f0714
Chris Kitching <chriskitching@linux.com>
2017-10-13 06:16:03 +0100

    A macro for dynamically invoking a function
    Self-modifying cmake code is clearly a great idea.

--- scripts/Utils.cmake
+++ scripts/Utils.cmake
@@ -4,3 +4,18 @@ macro(default_value NAME VALUE)
         set(${NAME} ${VALUE})
     endif()
 endmacro()
+
+# Invoke a function, macro, or command by name.
+# This is, clearly, completely insane. All args given are forwarded to the target routine.
+macro(dynamic_call FN_NAME)
+    if (NOT COMMAND ${FN_NAME})
+        message(FATAL_ERROR "No such function: ${FN_NAME}")
+    endif()
+
+    string(RANDOM SNAME)
+    set(SCRIPT_PATH "${CMAKE_BINARY_DIR}/${SNAME}.cmake")
+
+    file(WRITE ${SCRIPT_PATH} "${FN_NAME}(${ARGN})")
+    include(${SCRIPT_PATH})
+    file(REMOVE ${SCRIPT_PATH})
+endmacro()
