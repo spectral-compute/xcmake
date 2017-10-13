4b4a24ba064f877993a0944042b27197d6c9b337
Chris Kitching <chriskitching@linux.com>
2017-10-13 01:08:00 +0100

    Introduce docopt externalproj script
    It's a handy library, and a good testcase for ExternalProj.cmake

---
+++ dependencies/docopt-cpp.cmake
@@ -0,0 +1,9 @@
+include(ExternalProj)
+
+AddExternalProject(docopt_proj
+    GIT_REPOSITORY https://github.com/docopt/docopt.cpp.git
+    GIT_TAG v0.6.2
+    CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+    INSTALL_COMMAND make install && cp -RfT <INSTALL_DIR>/lib64 <INSTALL_DIR>/lib/ || true
+    STATIC_LIBRARIES docopt
+)
