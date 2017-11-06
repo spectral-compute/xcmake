5bf59ba9c5f78ffe644d8cd9ceb9fa71ddef262e
Chris Kitching <chriskitching@linux.com>
2017-11-06 03:01:57 +0000

    The usual CMake RPATH boilerplate...

--- scripts/Init.cmake
+++ scripts/Init.cmake
@@ -44,6 +44,11 @@ else()
     message(FATAL_ERROR "Unsupported build type: ${CMAKE_BUILD_TYPE}")
 endif()
 
+# Saneify CMake's RPATH handling...
+set(CMAKE_SKIP_BUILD_RPATH FALSE)
+set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+set(CMAKE_INSTALL_RPATH "./lib")
 
 # Include the rest of xcmake, for convenience.
 include(ArgHandle)
