7a3d0ae51ec2e0cc32e3ac64111972a84b5b4d85
Nicholas Tomlinson <nstomlinson@gmail.com>
2018-05-12 00:24:03 +0100

    Make add_headers only install headers

--- scripts/Headers.cmake
+++ scripts/Headers.cmake
@@ -5,5 +5,6 @@ function(add_headers TARGET)
     cmake_parse_arguments("h" "${flags}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     # TODO: Partial preprocessing and all that jazz...
-    install(DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/${h_HEADER_PATH}/ DESTINATION ./include/${h_INSTALL_DESTINATION})
+    install(DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/${h_HEADER_PATH}/ DESTINATION ./include/${h_INSTALL_DESTINATION}
+            FILES_MATCHING REGEX "\\.h(pp)?$")
 endfunction()
