3d1d400391358ae4529e322b42e0b8c100b653b5
Chris Kitching <chriskitching@linux.com>
2018-05-12 22:35:01 +0100

    Make XCMake's gtest importer tolerant of gtest injection

--- scripts/GTest.cmake
+++ scripts/GTest.cmake
@@ -25,30 +25,38 @@ function(add_gtest_executable TARGET)
     remove_argument(FLAG EXTRA_ARGS CUSTOM_MAIN)
     add_test_executable(${TARGET} ${EXTRA_ARGS})
 
-    # Find and add GoogleTest/GoogleMock.
-    # GTest provides nifty imported targets (which handle the threads dependency for us), but
-    # gmock does not appear to...
-    find_package(GTest REQUIRED)
-    find_library(GMock_LIB gmock)
-    find_path(GMock_INC gmock/gmock.h)
-    if (NOT GMock_LIB OR NOT GMock_INC)
-        message(FATAL_ERROR "Unable to find gmock library or headers :(")
+    # If gtest/gmock targets already exist, for any reason, just use them instead of searching for new ones.
+    if (NOT TARGET gtest)
+        # Find and add GoogleTest/GoogleMock.
+        # GTest provides nifty imported targets (which handle the threads dependency for us), but
+        # gmock does not appear to...
+        find_package(GTest REQUIRED)
+        find_library(GMock_LIB gmock)
+        find_path(GMock_INC gmock/gmock.h)
+        if (NOT GMock_LIB OR NOT GMock_INC)
+            message(FATAL_ERROR "Unable to find gmock library or headers :(")
+        endif()
+
+        find_library(gmock_main gmock_main)
+        if (NOT GMock_MAIN)
+            message(FATAL_ERROR "Unable to find gmock_main library :(")
+        endif ()
+
+        # Add gmock include
+        target_include_directories(${TARGET} SYSTEM PRIVATE ${GMock_INC})
+
+        set(gtest GTest::GTest)
+    else()
+        set(gtest gtest)
+        set(gmock gmock)
+        set(gmock_main gmock_main)
     endif()
 
-    # Add gmock include
-    target_include_directories(${TARGET} SYSTEM PRIVATE ${GMock_INC})
-
-    # Apply gtest imported targets (which hopefully take care of include paths for us...)
-    target_link_libraries(${TARGET} PRIVATE GTest::GTest)
+    target_link_libraries(${TARGET} PRIVATE ${gtest})
 
     # Add the default main, if required. We must use the one from gmock, not gtest, since we want
     # features from gmock and the gtest main doesn't initialise gmock.
     if (NOT "${gt_CUSTOM_MAIN}")
-        find_library(GMock_MAIN gmock_main)
-        if (NOT GMock_MAIN)
-            message(FATAL_ERROR "Unable to find gmock_main library :(")
-        endif()
-
-        target_link_libraries(${TARGET} PRIVATE ${GMock_MAIN})
+        target_link_libraries(${TARGET} PRIVATE ${gmock_main})
     endif()
 endfunction(add_gtest_executable)
