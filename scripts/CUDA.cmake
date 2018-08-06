579f36baa637d6119c4649796a6c67d1ba98461e
Chris Kitching <chriskitching@linux.com>
2018-08-06 14:07:24 +0100

    Colour-code CUDA decection messages by vendor

--- scripts/CUDA.cmake
+++ scripts/CUDA.cmake
@@ -10,7 +10,7 @@ function(configure_for_nvidia TARGET)
         message(FATAL_ERROR "CUDA 9 harms performance and is therefore not supported. Please use CUDA 8")
     endif()
 
-    message_colour(STATUS Yellow "Using CUDA ${CUDA_VERSION_STRING} from ${CUDA_TOOLKIT_ROOT_DIR}")
+    message_colour(STATUS BoldGreen "Using NVIDIA CUDA ${CUDA_VERSION_STRING} from ${CUDA_TOOLKIT_ROOT_DIR}")
 
     get_target_property(SOURCE_FILES ${TARGET} SOURCES)
 
@@ -36,6 +36,7 @@ endfunction()
 # Set up an AMD CUDA target.
 function(configure_for_amd TARGET)
     find_package(Scale REQUIRED)
+    message_colour(STATUS BoldRed "Found support for CUDA on AMD in ${SCALE_AMD_TOOLKIT_ROOT_DIR}")
 
     get_target_property(SOURCE_FILES ${TARGET} SOURCES)
 
