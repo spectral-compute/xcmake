24b6cd288b278586118f3417458f6bc88043dc65
Chris Kitching <chriskitching@linux.com>
2018-08-18 00:46:03 +0100

    Add SOURCE_COVERAGE interface target
    Although this turns out to be of almost no use because the PGO
    system (and associated gubbins) doesn't work at all for GPUs.

---
+++ scripts/properties/SOURCE_COVERAGE.cmake
@@ -0,0 +1,8 @@
+define_xcmake_target_property(
+    SOURCE_COVERAGE FLAG
+    BRIEF_DOCS "Enable Clang's source-level overage report generation."
+    DEFAULT OFF
+)
+target_compile_options(SOURCE_COVERAGE_EFFECTS INTERFACE
+    -fprofile-instr-generate -fcoverage-mapping
+)
