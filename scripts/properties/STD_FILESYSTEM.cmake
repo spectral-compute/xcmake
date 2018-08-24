9d3a8127210a775b7c177d7dae9d4e345bd60265
Chris Kitching <chriskitching@linux.com>
2018-08-24 03:03:52 +0100

    Add target property for enabling std::filesystem

---
+++ scripts/properties/STD_FILESYSTEM.cmake
@@ -0,0 +1,10 @@
+define_xcmake_target_property(
+    STD_FILESYSTEM FLAG
+    BRIEF_DOCS "Target uses std::filesystem."
+    DEFAULT OFF
+)
+
+# TODO: Once we add libc++ support, this wants to be a generator expression that turns it into a no-op when using libc++.
+set_target_properties(STD_FILESYSTEM_EFFECTS
+    PROPERTIES INTERFACE_LINK_LIBRARIES stdc++fs
+)
