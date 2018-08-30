d5da355722e120d8dd1b657337188218cda5ab23
Chris Kitching <chriskitching@linux.com>
2018-08-30 10:32:51 +0100

    Be slightly more forgiving to test targets w.r.t warnings

--- scripts/Test.cmake
+++ scripts/Test.cmake
@@ -1,27 +1,29 @@
-# Add a test executable (installed under ./test)
-function(add_test_executable TARGET)
-    add_executable(${TARGET} ${ARGN} NOINSTALL)
-
+# Do stuff common to all test targets.
+macro(configure_test_target TARGET)
     install(
         TARGETS ${TARGET}
         RUNTIME DESTINATION test/bin
         LIBRARY DESTINATION test/lib
     )
 
+    # Tune the warnings that nobody cares about in test code down a wee bit.
+    target_compile_options(${TARGET} PRIVATE
+        -Wno-weak-vtables
+        -Wno-missing-variable-declarations
+    )
+
     # TODO: Could be an overridden install(), but holy crap that's complicated.
     set_target_properties(${TARGET} PROPERTIES INSTALL_RPATH "$ORIGIN/../lib;$ORIGIN/../../lib")
+endmacro()
+
+# Add a test executable (installed under ./test)
+function(add_test_executable TARGET)
+    add_executable(${TARGET} ${ARGN} NOINSTALL)
+    configure_test_target(${TARGET})
 endfunction()
 
 # Add a test library (installed under ./test)
 function(add_test_library TARGET)
     add_library(${TARGET} ${ARGN} NOINSTALL)
-
-    install(
-        TARGETS ${TARGET}
-        RUNTIME DESTINATION test/bin
-        LIBRARY DESTINATION test/lib
-    )
-
-    # TODO: Could be an overridden install(), but holy crap that's complicated.
-    set_target_properties(${TARGET} PROPERTIES INSTALL_RPATH "$ORIGIN/../lib;$ORIGIN/../../lib")
+    configure_test_target(${TARGET})
 endfunction()
