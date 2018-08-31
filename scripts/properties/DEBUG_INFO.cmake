9c17078c00d06659d787ba70921b871acb13fb7a
Chris Kitching <chriskitching@linux.com>
2018-08-31 14:17:05 +0100

    Add target property to control emission of debug info

---
+++ scripts/properties/DEBUG_INFO.cmake
@@ -0,0 +1,17 @@
+if ("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
+    set(DEFAULT_DEB "ON")
+else()
+    set(DEFAULT_DEB "OFF")
+endif()
+
+define_xcmake_target_property(
+    DEBUG_INFO FLAG
+    BRIEF_DOCS "Add debug information to the binary"
+    DEFAULT ${DEFAULT_DEB}
+)
+target_compile_options(DEBUG_INFO_EFFECTS INTERFACE
+    -gcolumn-info
+    -fdebug-macro
+    -fno-limit-debug-info
+    -glldb
+)
