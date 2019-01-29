526b4fc9257897ba26a5e7c57c99e06e8e66b28b
Chris Kitching <chriskitching@linux.com>
2019-01-29 20:13:36 +0000

    Move FFTW find-module into xcmake

---
+++ dependencies/Findfftw.cmake
@@ -0,0 +1,12 @@
+find_path(fftw_INCLUDE_DIRS fftw3.h)
+find_library(fftw_LIBRARY_d fftw3)
+find_library(fftw_LIBRARY_f fftw3f)
+
+set(fftw_LIBRARIES ${fftw_LIBRARY_d} ${fftw_LIBRARY_f})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(fftw DEFAULT_MSG fftw_INCLUDE_DIRS fftw_LIBRARIES)
+
+if (fftw_FOUND)
+    message("Found FFTW:\n     Includes: ${fftw_INCLUDE_DIRS}\n     Libraries: ${fftw_LIBRARIES}")
+endif()
