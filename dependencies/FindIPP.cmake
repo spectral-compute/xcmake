db4c87402fc9ad973c7faa233ea0614023622327
Nicholas Tomlinson <nstomlinson@gmail.com>
2019-02-03 18:11:44 +0000

    Make it possible to use the IPP finder script twice in the same project

--- dependencies/FindIPP.cmake
+++ dependencies/FindIPP.cmake
@@ -9,21 +9,25 @@ find_path(
 )
 
 macro(find_ipp_lib SILLY_NAME PRETTY_NAME)
-    find_library(
-        IPP_${PRETTY_NAME} ipp${SILLY_NAME}
-        PATHS /opt
-        PATH_SUFFIXES ${INTEL_PATH}/linux/ipp/lib/intel64_lin
-    )
-
-    if (IPP_${PRETTY_NAME} AND NOT TARGET IPP::${PRETTY_NAME})
-        add_library(IPP::${PRETTY_NAME} SHARED IMPORTED GLOBAL)
-
-        set_target_properties(IPP::${PRETTY_NAME} PROPERTIES
-            INTERFACE_INCLUDE_DIRECTORIES "${IPP_INCLUDE_DIR}"
-            IMPORTED_LOCATION "${IPP_${PRETTY_NAME}}"
+    if (TARGET IPP::${PRETTY_NAME})
+        set(IPP_${PRETTY_NAME}_FOUND ON)
+    else ()
+        find_library(
+            IPP_${PRETTY_NAME} ipp${SILLY_NAME}
+            PATHS /opt
+            PATH_SUFFIXES ${INTEL_PATH}/linux/ipp/lib/intel64_lin
         )
 
-        set(IPP_${PRETTY_NAME}_FOUND ON)
+        if (IPP_${PRETTY_NAME})
+            add_library(IPP::${PRETTY_NAME} SHARED IMPORTED GLOBAL)
+
+            set_target_properties(IPP::${PRETTY_NAME} PROPERTIES
+                INTERFACE_INCLUDE_DIRECTORIES "${IPP_INCLUDE_DIR}"
+                IMPORTED_LOCATION "${IPP_${PRETTY_NAME}}"
+            )
+
+            set(IPP_${PRETTY_NAME}_FOUND ON)
+        endif()
     endif()
 endmacro()
 
