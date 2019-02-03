7f4fa517cd65d2038cd7ff29034e3db213a712ac
Nicholas Tomlinson <nstomlinson@gmail.com>
2019-02-03 18:11:54 +0000

    Make it possible to use the PCRE2 finder script twice in the same project

--- dependencies/FindPCRE2.cmake
+++ dependencies/FindPCRE2.cmake
@@ -6,12 +6,14 @@ foreach(_C 8 16 32)
     find_library(PCRE2_C${_C} pcre2-${_C})
 
     if (PCRE2_C${_C})
-        add_library(PCRE2::C${_C} SHARED IMPORTED GLOBAL)
+        if (NOT TARGET PCRE2::C${_C})
+            add_library(PCRE2::C${_C} SHARED IMPORTED GLOBAL)
 
-        set_target_properties(PCRE2::C${_C} PROPERTIES
-            INTERFACE_INCLUDE_DIRECTORIES "${PCRE2_INCLUDE_DIR}"
-            IMPORTED_LOCATION "${PCRE2_C${_C}}"
-        )
+            set_target_properties(PCRE2::C${_C} PROPERTIES
+                INTERFACE_INCLUDE_DIRECTORIES "${PCRE2_INCLUDE_DIR}"
+                IMPORTED_LOCATION "${PCRE2_C${_C}}"
+            )
+        endif()
         set(PCRE2_C${_C}_FOUND ON)
     endif()
 endforeach()
