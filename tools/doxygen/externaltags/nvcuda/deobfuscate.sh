933e970ff9dbf42a3c13b4890e815ea466a3d063
Chris Kitching <chriskitching@linux.com>
2019-02-23 16:20:47 +0000

    Add a mechanism for cross-referencing NVIDIA's doxygen manual

---
+++ tools/doxygen/externaltags/nvcuda/deobfuscate.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Reverse nvidia's doxygen tag obfuscation.
+cat $1 | sed -n -Ee '1h;1!H;${;g;s|\s+<anchorfile>([a-zA-Z0-9_]+).html</anchorfile>\n\s+<anchor>([a-z0-9])[a-z0-9]([a-z0-9]+)|\n      <anchorfile>\1.html</anchorfile>\n      <anchor>\1_1\2\3|g;p;}' > $2
