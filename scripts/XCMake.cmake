9a623464cc033d1f7e40e80b3a99b9b9700c2132
Benjamin Knight <bknight4125@gmail.com>
2019-05-02 15:50:07 +0000

    Remove nonfunctional code

--- scripts/XCMake.cmake
+++ scripts/XCMake.cmake
@@ -1,15 +1,3 @@
 ## Include the rest of xcmake, for convenience.
-#include(ArgHandle)
-#include(ExternalProj)
-#include(Headers)
-#include(CUDA)
-#include(Test)
-#include(GTest)
-#include(Export)
-#include(ScopedSubdirs)
-#include(OnExit)
-#include(Summary)
-#include(IncludeGuard)
-#include(Doxygen)
 
 include(Properties)
