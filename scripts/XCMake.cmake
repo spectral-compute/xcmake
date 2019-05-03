e0f17c43edfac9fbe4d50b69d76e004a340f27f4
Benjamin Knight <bknight4125@gmail.com>
2019-05-03 20:56:55 +0000

    Include our external project related code at the proper time

--- scripts/XCMake.cmake
+++ scripts/XCMake.cmake
@@ -2,3 +2,5 @@
 
 include(Properties)
 include(Flags)
+include(ExternalProj) # Can't be in Init.cmake because content such as CMAKE_STATIC_LIBRARY_SUFFIX are set by Project() call
+include(GTest) # Depends on ExternalProj through Ext_GoogleTest.cmake
