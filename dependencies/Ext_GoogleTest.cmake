a10d0cc328cfa9fdfa8ad71cfcc5e76ce7b17461
Chris Kitching <chriskitching@linux.com>
2019-05-04 04:22:38 +0100

    Build googletest as shared libraries
    Using static libraries creates issues when multiple googletest
    consumers are linked together. It also uses slightly more memory...

--- dependencies/Ext_GoogleTest.cmake
+++ dependencies/Ext_GoogleTest.cmake
@@ -5,9 +5,9 @@ set(GTEST_TAG master CACHE STRING "Allow user to set the GTEST external project'
 AddExternalProject(googletest
   GIT_REPOSITORY    git@gitlab.com:spectral-ai/engineering/thirdparty/googletest
   GIT_TAG           ${GTEST_TAG}
-  CMAKE             TRUE
+  CMAKE_ARGS        -DBUILD_SHARED_LIBS=ON
   # Install libraries to /lib not /lib64. There's prooobably a more elegant solution?
   INSTALL_COMMAND make install && cp -RfT <INSTALL_DIR>/lib64 <INSTALL_DIR>/lib/ || true
 
-  STATIC_LIBRARIES  gtest gmock gtest_main gmock_main
+  SHARED_LIBRARIES  gtest gmock gtest_main gmock_main
 )
