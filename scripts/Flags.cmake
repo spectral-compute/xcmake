a49bb441dea031759c90227010854d9e114aad4b
Chris Kitching <chriskitching@linux.com>
2019-05-04 03:10:35 +0100

    Remove debug code from Flags.cmake

--- scripts/Flags.cmake
+++ scripts/Flags.cmake
@@ -8,8 +8,6 @@ option(XCMAKE_ENABLE_TESTS "Build unit tests for all projects" ON)
 # Add project-specific toggles
 string(TOUPPER ${PROJECT_NAME} NAME) # Get it uppercase to remain consistent
 option(${NAME}_ENABLE_DOCS "Build the documentation for this project" ${XCMAKE_ENABLE_DOCS})
-message(${NAME}_ENABLE_DOCS " local flag is: " ${${NAME}_ENABLE_DOCS})
-
 option(${NAME}_ENABLE_TESTS "Build the units tests for this project" ${XCMAKE_ENABLE_TESTS})
 
 # Function to check a given project for flag state
