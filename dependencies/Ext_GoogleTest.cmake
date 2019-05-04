64c4ac3ba053663ac4e96da85a75f21d0e979ead
Chris Kitching <chriskitching@linux.com>
2019-05-04 03:17:31 +0100

    Install googletest libraries to /lib not /lib64, similar to docopt
    This causes them to appear where they're assumed to by by the
    EP-wrapper script, making buildsystem dependencies actually work.

--- dependencies/Ext_GoogleTest.cmake
+++ dependencies/Ext_GoogleTest.cmake
@@ -4,5 +4,8 @@ AddExternalProject(googletest
   GIT_REPOSITORY    git@gitlab.com:spectral-ai/engineering/thirdparty/googletest
   GIT_TAG           ${GTEST_TAG}
   CMAKE             TRUE
+  # Install libraries to /lib not /lib64. There's prooobably a more elegant solution?
+  INSTALL_COMMAND make install && cp -RfT <INSTALL_DIR>/lib64 <INSTALL_DIR>/lib/ || true
+
   STATIC_LIBRARIES  gtest gmock gtest_main gmock_main
 )
