a8e1b8e198a1ae3a6923363f5c31be847078e440
Benjamin Knight <bknight4125@gmail.com>
2019-05-07 04:29:00 +0000

    Include ExternalProj in GoogleTest.cmake

--- dependencies/externals/GoogleTest.cmake
+++ dependencies/externals/GoogleTest.cmake
@@ -2,6 +2,8 @@ SubdirectoryGuard(GTest)
 
 set(GTEST_TAG master CACHE STRING "Allow user to set the GTEST external project's checkout tag" "master")
 
+include(ExternalProj)
+
 AddExternalProject(googletest
   GIT_REPOSITORY    git@gitlab.com:spectral-ai/engineering/thirdparty/googletest
   GIT_TAG           ${GTEST_TAG}
