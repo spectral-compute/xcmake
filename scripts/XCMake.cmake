f107b44e070b1e6f1e609e425a1129977ce33178
Benjamin Knight <bknight4125@gmail.com>
2019-05-07 04:29:00 +0000

    Include ExternalProj in GoogleTest.cmake

--- scripts/XCMake.cmake
+++ scripts/XCMake.cmake
@@ -2,5 +2,4 @@
 
 include(Properties)
 include(Flags)
-include(ExternalProj) # Can't be in Init.cmake because content such as CMAKE_STATIC_LIBRARY_SUFFIX are set by Project() call
-include(GTest) # Depends on ExternalProj through GoogleTest.cmake
+include(GTest)
