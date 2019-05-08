d25580d674997672f166db44589c003ee7c97cc9
Benjamin Knight <bknight4125@gmail.com>
2019-05-08 01:19:47 +0000

    Use a more unique name to avoid possible clashes

--- scripts/Flags.cmake
+++ scripts/Flags.cmake
@@ -6,9 +6,9 @@ option(XCMAKE_ENABLE_DOCS "Generate documentation for all projects" ON)
 option(XCMAKE_ENABLE_TESTS "Build unit tests for all projects" ON)
 
 # Add project-specific toggles
-string(TOUPPER ${PROJECT_NAME} NAME) # Get it uppercase to remain consistent
-option(${NAME}_ENABLE_DOCS "Build the documentation for this project" ${XCMAKE_ENABLE_DOCS})
-option(${NAME}_ENABLE_TESTS "Build the units tests for this project" ${XCMAKE_ENABLE_TESTS})
+string(TOUPPER ${PROJECT_NAME} PNAME_UPPER) # Get it uppercase to remain consistent
+option(${PNAME_UPPER}_ENABLE_DOCS "Build the documentation for this project" ${XCMAKE_ENABLE_DOCS})
+option(${PNAME_UPPER}_ENABLE_TESTS "Build the units tests for this project" ${XCMAKE_ENABLE_TESTS})
 
 # Aborts the calling function if the desired docs aren't turned on
 # PROJECT - The project flag to check
@@ -24,7 +24,7 @@ macro(ensure_docs_enabled)
     string(TOUPPER ${f_PROJECT} PROJECT_U)
     if (NOT ${PROJECT_U}_ENABLE_DOCS)
         message_colour(STATUS BoldYellow
-                "Not building ${f_TYPE} for ${NAME} because ${PROJECT_U}_ENABLE_DOCS == OFF")
+                "Not building ${f_TYPE} for ${PNAME_UPPER} because ${PROJECT_U}_ENABLE_DOCS == OFF")
         set(FLAGCHECK FALSE PARENT_SCOPE)
         return ()
     endif ()
