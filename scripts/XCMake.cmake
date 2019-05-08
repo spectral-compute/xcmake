c0475d9e244ca60ad601385f59019e6de39d201a
Benjamin Knight <bknight4125@gmail.com>
2019-05-08 02:30:40 +0000

    Set upper and lowercase versions globally for convenience

--- scripts/XCMake.cmake
+++ scripts/XCMake.cmake
@@ -1,5 +1,8 @@
-## Include the rest of xcmake, for convenience.
+# Provide uppercase and lowercase versions of PROJECT_NAME
+string(TOUPPER ${PROJECT_NAME} XCMAKE_PROJECT_NAME_UPPER)
+string(TOLOWER ${PROJECT_NAME} XCMAKE_PROJECT_NAME_LOWER)
 
+## Include the rest of xcmake, for convenience.
 include(Properties)
 include(Flags)
 include(GTest)
