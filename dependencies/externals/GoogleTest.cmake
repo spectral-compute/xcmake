e8cd40ae13e5dada28d8f0c6436e9540182e0a6e
Benjamin Knight <bknight4125@gmail.com>
2019-05-09 17:25:58 +0000

    Don't include INSTALL_COMMAND within CMAKE_ARGS

--- dependencies/externals/GoogleTest.cmake
+++ dependencies/externals/GoogleTest.cmake
@@ -12,10 +12,9 @@ else ()
     AddExternalProject(googletest
       GIT_REPOSITORY    git@gitlab.com:spectral-ai/engineering/thirdparty/googletest
       GIT_TAG           ${GTEST_TAG}
-      CMAKE_ARGS        -DBUILD_SHARED_LIBS=ON
       # Install libraries to /lib not /lib64. There's prooobably a more elegant solution?
       INSTALL_COMMAND make install && cp -RfT <INSTALL_DIR>/lib64 <INSTALL_DIR>/lib/ || true
-
+      CMAKE_ARGS        -DBUILD_SHARED_LIBS=ON
       SHARED_LIBRARIES  gtest gmock gtest_main gmock_main
     )
 endif ()
