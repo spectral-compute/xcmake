78c08785b334e858f499d96bc6519d70ef7ba4aa
Nicholas Tomlinson <nstomlinson@gmail.com>
2019-05-09 17:07:30 +0100

    Make it possible to use a system installed gtest
    In addition to making it possible to use such a gtest when the
    external project is broken, this saves some compile time and can be
    useful for debugging the case where the external project has a bug
    that upstream does not.

--- dependencies/externals/GoogleTest.cmake
+++ dependencies/externals/GoogleTest.cmake
@@ -4,12 +4,18 @@ set(GTEST_TAG master CACHE STRING "Allow user to set the GTEST external project'
 
 include(ExternalProj)
 
-AddExternalProject(googletest
-  GIT_REPOSITORY    git@gitlab.com:spectral-ai/engineering/thirdparty/googletest
-  GIT_TAG           ${GTEST_TAG}
-  CMAKE_ARGS        -DBUILD_SHARED_LIBS=ON
-  # Install libraries to /lib not /lib64. There's prooobably a more elegant solution?
-  INSTALL_COMMAND make install && cp -RfT <INSTALL_DIR>/lib64 <INSTALL_DIR>/lib/ || true
+option(XCMAKE_SYSTEM_GTEST "Use system gtest rather than build our own" Off)
 
-  SHARED_LIBRARIES  gtest gmock gtest_main gmock_main
-)
+if (XCMAKE_SYSTEM_GTEST)
+    find_package(GTest)
+else ()
+    AddExternalProject(googletest
+      GIT_REPOSITORY    git@gitlab.com:spectral-ai/engineering/thirdparty/googletest
+      GIT_TAG           ${GTEST_TAG}
+      CMAKE_ARGS        -DBUILD_SHARED_LIBS=ON
+      # Install libraries to /lib not /lib64. There's prooobably a more elegant solution?
+      INSTALL_COMMAND make install && cp -RfT <INSTALL_DIR>/lib64 <INSTALL_DIR>/lib/ || true
+
+      SHARED_LIBRARIES  gtest gmock gtest_main gmock_main
+    )
+endif ()
