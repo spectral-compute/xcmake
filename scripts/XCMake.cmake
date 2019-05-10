580da096cb2998cd6bb1ec7ce920cd6e76ddada1
Chris Kitching <chriskitching@linux.com>
2019-05-10 15:52:10 +0100

    Include GTest.cmake in the "include everything" cmake script

--- scripts/XCMake.cmake
+++ scripts/XCMake.cmake
@@ -3,5 +3,6 @@ string(TOUPPER ${PROJECT_NAME} XCMAKE_PROJECT_NAME_UPPER)
 string(TOLOWER ${PROJECT_NAME} XCMAKE_PROJECT_NAME_LOWER)
 
 ## Include the rest of xcmake, for convenience.
+include(GTest)
 include(Properties)
 include(Flags)
