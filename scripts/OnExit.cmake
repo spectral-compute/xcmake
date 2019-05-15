46a2e44cc40c063f998336cddc1d69db1a66de5f
Chris Kitching <chriskitching@linux.com>
2019-05-15 18:14:22 +0100

    Clarify some rationale for the AddExitFunction insanity

--- scripts/OnExit.cmake
+++ scripts/OnExit.cmake
@@ -2,7 +2,10 @@
 #
 # This relies on the undocumented fact that cmake checks CMAKE_BACKWARDS_COMPATIBILITY *after* running the scripts
 # fully. It will certainly always check that varibale at some point, so new versions of cmake are unlikely to stop
-# your hook from running at all: but it might stop happening at the end.
+# your hook from running at all: but it might stop happening at the end. Since that variable has been deprecated for
+# years (in fact it's very nearly the *first* thing to have been deprecated by cmake), it is very unlikely a user is
+# going to touch the variable and trigger the variable monitor early (and if they do, we have much bigger problems
+# because it implies they're using cmake <= 2.4).
 #
 # Still, it does the job :D
 
