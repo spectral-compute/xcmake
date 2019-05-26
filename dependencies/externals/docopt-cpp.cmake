ad7b443490c48469884feddce34c4baea9c86748
Chris Kitching <chriskitching@linux.com>
2019-05-26 12:12:06 +0100

    Outrageous hack to work around argparse bug
    So _this_ is why we used to parse the entire EP signature.
    Oops.

--- dependencies/externals/docopt-cpp.cmake
+++ dependencies/externals/docopt-cpp.cmake
@@ -4,7 +4,7 @@ include(ExternalProj)
 AddExternalProject(docopt_proj
     GIT_REPOSITORY https://github.com/docopt/docopt.cpp.git
     GIT_TAG v0.6.2
-    CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
     INSTALL_COMMAND make install && cp -RfT <INSTALL_DIR>/lib64 <INSTALL_DIR>/lib/ || true
+    CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
     STATIC_LIBRARIES docopt
 )
