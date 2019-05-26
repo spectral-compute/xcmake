0299a1aaf4815766cb02b7a635098869529d7ec3
Chris Kitching <chriskitching@linux.com>
2019-05-26 11:37:44 +0100

    Make xcmake correctly handle non-CUDA compilation
    Not that we need this very often, but it is funny that it's been
    broken for a year and nobody noticed until now.

--- scripts/CUDA.cmake
+++ scripts/CUDA.cmake
@@ -1,13 +1,15 @@
 function(add_cuda_to_target TARGET)
-    if ("${XCMAKE_GPU_TYPE}" STREQUAL "amd")
-    elseif ("${XCMAKE_GPU_TYPE}" STREQUAL "nvidia")
-    elseif ("${XCMAKE_GPU_TYPE}" STREQUAL "")
-        message(FATAL_ERROR "You didn't specify any GPU targets with -DXCMAKE_GPUS!")
-    else ()
-        message(FATAL_ERROR "Unknown GPU type: ${XCMAKE_GPU_TYPE}")
-    endif ()
+    if (XCMAKE_GPU_TYPE)
+        if ("${XCMAKE_GPU_TYPE}" STREQUAL "amd")
+        elseif ("${XCMAKE_GPU_TYPE}" STREQUAL "nvidia")
+        else ()
+            message(FATAL_ERROR "Unknown GPU type: ${XCMAKE_GPU_TYPE}")
+        endif ()
 
-    set_target_properties(${TARGET} PROPERTIES CUDA ON)
+        set_target_properties(${TARGET} PROPERTIES CUDA ON)
+    else()
+        message(FATAL_ERROR "You didn't specify any GPU targets with -DXCMAKE_GPUS, so CUDA targets are not supported.")
+    endif()
 endfunction()
 
 # Add an executable that uses CUDA.
