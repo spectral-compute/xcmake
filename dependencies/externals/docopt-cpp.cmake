f74ad20a790d6b935162e619abf6100d35a004d2
IceDreamer <bknight4125@gmail.com>
2019-07-05 01:41:30 +0100

    Use /EHsc exception handling mode for docopt project on Windows Microsoft exception handling is completely insane. Without this, it gets no mode, and complains that you can't use exceptions with exceptions disabled. Silly Windows. Additionally, remove BUILD_TYPE because our wrapper handles that now.

--- dependencies/externals/docopt-cpp.cmake
+++ dependencies/externals/docopt-cpp.cmake
@@ -1,10 +1,14 @@
 SubdirectoryGuard(Docopt)
 include(ExternalProj)
 
+if(WIN32)
+    set(DOCOPT_FLAGS "/EHsc")
+endif()
+
 AddExternalProject(docopt_proj
     GIT_REPOSITORY https://github.com/docopt/docopt.cpp.git
     GIT_TAG v0.6.2
     INSTALL_COMMAND make install && cp -RfT <INSTALL_DIR>/lib64 <INSTALL_DIR>/lib/ || true
-    CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+    CMAKE_ARGS "-DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} ${DOCOPT_FLAGS}"
     STATIC_LIBRARIES docopt
 )
