b1d1e21c1bd11f6a33716b7cab49bcfba1b852d6
IceDreamer <bknight4125@gmail.com>
2019-07-05 01:42:04 +0100

    Remove custom install command This should be fully handled by our install() wrapper now

--- dependencies/externals/docopt-cpp.cmake
+++ dependencies/externals/docopt-cpp.cmake
@@ -8,7 +8,6 @@ endif()
 AddExternalProject(docopt_proj
     GIT_REPOSITORY https://github.com/docopt/docopt.cpp.git
     GIT_TAG v0.6.2
-    INSTALL_COMMAND make install && cp -RfT <INSTALL_DIR>/lib64 <INSTALL_DIR>/lib/ || true
     CMAKE_ARGS "-DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} ${DOCOPT_FLAGS}"
     STATIC_LIBRARIES docopt
 )
