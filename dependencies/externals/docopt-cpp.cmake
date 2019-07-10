83c3d3542e4eca7cbbf974796789b0330af61563
IceDreamer <bknight4125@gmail.com>
2019-07-10 19:09:04 +0100

    Make sure we have an appropriate target for both docopt libraries Also add an explanation for /EHsc

--- dependencies/externals/docopt-cpp.cmake
+++ dependencies/externals/docopt-cpp.cmake
@@ -2,13 +2,17 @@ SubdirectoryGuard(Docopt)
 include(ExternalProj)
 
 set(DOCOPT_FLAGS "")
-if (WIN32)
-    set(DOCOPT_FLAGS "/EHsc")
+set(DOCOPT_STATIC_NAME "docopt")
+if(MSVC)
+    set(DOCOPT_FLAGS "/EHsc") # Sets the exception handling mode
+    set(DOCOPT_STATIC_NAME "docopt_s") # They manually set both names to "docopt" for nonwindows... Sigh.
 endif()
 
 AddExternalProject(docopt_proj
     GIT_REPOSITORY https://github.com/docopt/docopt.cpp.git
     GIT_TAG v0.6.2
     CMAKE_ARGS "-DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} ${DOCOPT_FLAGS}"
-    STATIC_LIBRARIES docopt
+    # docopt always builds both libraries, but their names differ by platform
+    STATIC_LIBRARIES ${DOCOPT_STATIC_NAME}
+    SHARED_LIBRARIES docopt
 )
