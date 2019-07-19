35d58c12f57123dbd4bfa484a8106e5189ca253a
Chris Kitching <chriskitching@linux.com>
2019-07-19 17:27:05 +0100

    Revert "Make sure we have an appropriate target for both docopt libraries"
    This reverts commit 83c3d3542e4eca7cbbf974796789b0330af61563.
    
    This broke Linux builds...

--- dependencies/externals/docopt-cpp.cmake
+++ dependencies/externals/docopt-cpp.cmake
@@ -2,19 +2,15 @@ SubdirectoryGuard(Docopt)
 include(ExternalProj)
 
 set(DOCOPT_FLAGS "")
-set(DOCOPT_STATIC_NAME "docopt")
-if(MSVC)
-    set(DOCOPT_FLAGS "/EHsc") # Sets the exception handling mode
-    set(DOCOPT_STATIC_NAME "docopt_s") # They manually set both names to "docopt" for nonwindows... Sigh.
+if (WIN32)
+    set(DOCOPT_FLAGS "/EHsc")
 endif()
 
 AddExternalProject(docopt_proj
     GIT_REPOSITORY https://github.com/docopt/docopt.cpp.git
     GIT_TAG v0.6.2
     CMAKE_ARGS "-DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} ${DOCOPT_FLAGS}"
-    # docopt always builds both libraries, but their names differ by platform
-    STATIC_LIBRARIES ${DOCOPT_STATIC_NAME}
-    SHARED_LIBRARIES docopt
+    STATIC_LIBRARIES docopt
 )
 ExternalProject_Add_Step(docopt_proj RUNCOMMAND
     COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/external_projects/inst/lib/docopt.dll ${CMAKE_BINARY_DIR}/external_projects/inst/bin
