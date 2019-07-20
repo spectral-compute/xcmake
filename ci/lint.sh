834585b51e80683d9278dcb58fe72686c78f076d
Nicholas Tomlinson <nstomlinson@gmail.com>
2019-07-20 03:58:38 +0100

    Make the cmake linter run on every file at once
    This is much faster.

--- ci/lint.sh
+++ ci/lint.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-RC=0
-for i in $(find $1 -name \*.cmake -o -name CMakeLists.txt); do
-    if ! cmakelint --filter=-whitespace/extra,-linelength --spaces=4 "$i"; then
-        RC=1
-    fi
-done
-
-exit $RC
+cmakelint --filter=-whitespace/extra,-linelength --spaces=4 $(find $1 -name \*.cmake -o -name CMakeLists.txt)
