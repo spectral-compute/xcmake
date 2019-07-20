09a3eebec097eade11e9dfc6fba5e8f26b6d17bc
Nicholas Tomlinson <nstomlinson@gmail.com>
2019-07-20 04:43:44 +0100

    Don't insist on all caps finder script names
    This doesn't seem to be consistent with the output of
    pacman -Ql cmake | grep -E 'Find.*'
    
    Likewise for the names of the variables in a finder script, which
    should match the name of the script.

--- ci/lint.sh
+++ ci/lint.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-cmakelint --filter=-whitespace/extra,-linelength --spaces=4 $(find $1 -name \*.cmake -o -name CMakeLists.txt)
+cmakelint --filter=-whitespace/extra,-linelength,-convention/filename,-package/stdargs --spaces=4 \
+    $(find $1 -name \*.cmake -o -name CMakeLists.txt)
