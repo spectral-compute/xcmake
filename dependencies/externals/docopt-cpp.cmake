8dff0486a8ee5e933533f64045a21126260cbc6d
Benjamin Knight <bknight4125@gmail.com>
2019-07-25 02:02:01 +0000

    Always create a shared library target for docopt The docopt project builds two libraries. docopt for shared, and docopt_s for static. On non-MSVC, it then sets the output name for both to docopt. This name changing around presents problems, as our code generally assumes that the extensionless name can be used as our imported library target name, which isn't the case here. Much easier to just build shared all the time, and keep to a single matching target/library name.

--- dependencies/externals/docopt-cpp.cmake
+++ dependencies/externals/docopt-cpp.cmake
@@ -10,7 +10,7 @@ add_external_project(docopt_proj
     GIT_REPOSITORY https://github.com/docopt/docopt.cpp.git
     GIT_TAG v0.6.2
     CMAKE_ARGS "-DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} ${DOCOPT_FLAGS}"
-    STATIC_LIBRARIES docopt
+    SHARED_LIBRARIES docopt
 )
 if(XCMAKE_IMPLIB_PLATFORM)
     externalproject_add_step(docopt_proj RUNCOMMAND
