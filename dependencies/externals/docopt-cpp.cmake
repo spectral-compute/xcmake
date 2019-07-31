15fce062b990116086f9751170a17402f9e0a268
Michael Søndergaard <ms@spectralcompute.co.uk>
2019-07-31 11:17:06 +0200

    Use a more recent docopt-cpp
    The most recently released tag does not build with msvc, due to lacking
    an include of stdexcept [upstream ref](https://github.com/docopt/docopt.cpp/commit/72a8e3e01effe22ac0f4e29c14153743172efcb5).

--- dependencies/externals/docopt-cpp.cmake
+++ dependencies/externals/docopt-cpp.cmake
@@ -8,7 +8,7 @@ endif()
 
 add_external_project(docopt_proj
     GIT_REPOSITORY https://github.com/docopt/docopt.cpp.git
-    GIT_TAG v0.6.2
+    GIT_TAG 72a8e3e01effe22ac0f4e29c14153743172efcb5
     CMAKE_ARGS "-DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} ${DOCOPT_FLAGS}"
     SHARED_LIBRARIES docopt
 )
