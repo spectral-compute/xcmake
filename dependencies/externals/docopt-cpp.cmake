a2df1f2386b693422537be16f6baf38de70f41a4
IceDreamer <bknight4125@gmail.com>
2019-08-21 19:25:07 +0100

    Never build DOCOPT in Debug mode to avoid segfault

--- dependencies/externals/docopt-cpp.cmake
+++ dependencies/externals/docopt-cpp.cmake
@@ -1,6 +1,11 @@
 include_guard(GLOBAL)
 include(ExternalProj)
 
+set(DOCOPT_BUILD_TYPE ${CMAKE_BUILD_TYPE})
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    set(DOCOPT_BUILD_TYPE "RelWithDebInfo")
+endif()
+
 set(DOCOPT_FLAGS "")
 if(MSVC)
     set(DOCOPT_FLAGS "/EHsc") # Sets the exception handling mode
@@ -9,9 +14,12 @@ endif()
 add_external_project(docopt_proj
     GIT_REPOSITORY https://github.com/docopt/docopt.cpp.git
     GIT_TAG 72a8e3e01effe22ac0f4e29c14153743172efcb5
-    CMAKE_ARGS "-DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} ${DOCOPT_FLAGS}"
+    CMAKE_ARGS
+        "-DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} ${DOCOPT_FLAGS}"
+        "-DCMAKE_BUILD_TYPE=${DOCOPT_BUILD_TYPE}" # DOCOPT has a bug when built in Debug mode that causes a segfault in resulting exes
     SHARED_LIBRARIES docopt
 )
+
 if(XCMAKE_IMPLIB_PLATFORM)
     externalproject_add_step(docopt_proj RUNCOMMAND
         COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/external_projects/inst/lib/docopt.dll ${CMAKE_BINARY_DIR}/external_projects/inst/bin
