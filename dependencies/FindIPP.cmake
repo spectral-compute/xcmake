ba8a49dce5c121f694ac37434fe09aff2216f654
Chris Kitching <chriskitching@linux.com>
2019-08-29 17:01:00 +0100

    Make FindIPP platform-agnostic

--- dependencies/FindIPP.cmake
+++ dependencies/FindIPP.cmake
@@ -1,11 +1,30 @@
 include(FindPackageHandleStandardArgs)
 
-file(GLOB INTEL_PATH RELATIVE /opt/ CONFIGURE_DEPENDS "/opt/intel/compilers_and_libraries*")
+# Intel seems to enjoy hide-and-seek. To be maximally helpful, we'll provide platform-specific defaults that map to
+# the default install locations. Users can explicitly set @
+if (WIN32)
+    default_cache_value(IPP_ROOT "C:/Program Files (x86)/IntelSWTools" CACHE STRING "Directory containing the compilers_and_libraries* directory for IPP")
+    set(IPP_PLATFORM "windows")
+else()
+    default_cache_value(IPP_ROOT "/opt/intel/" CACHE STRING "Directory containing the compilers_and_libraries* directory for IPP")
+    set(IPP_PLATFORM "linux")
+endif()
+
+# Consistency is overrated.
+string(SUBSTRING ${IPP_PLATFORM} 0 3 IPP_PLATFORM_SHORT)
+
+# Dig into the version-specific one, naively.
+file(GLOB INTEL_PATH CONFIGURE_DEPENDS "${IPP_ROOT}/compilers_and_libraries*")
+if ("${INTEL_PATH}" STREQUAL "")
+    message(FATAL_ERROR "Intel IPP installation not found in \"${IPP_ROOT}\". Maybe you need to set `IPP_ROOT`?")
+endif()
+
+list(GET INTEL_PATH 0 INTEL_PATH)
+set(IPP_PATH "${INTEL_PATH}/${IPP_PLATFORM}/ipp")
 
 find_path(
     IPP_INCLUDE_DIR ipp.h
-    PATHS /opt
-    PATH_SUFFIXES ${INTEL_PATH}/linux/ipp/include
+    PATHS "${IPP_PATH}/include"
 )
 
 macro(find_ipp_lib SILLY_NAME PRETTY_NAME)
@@ -14,8 +33,7 @@ macro(find_ipp_lib SILLY_NAME PRETTY_NAME)
     else ()
         find_library(
             IPP_${PRETTY_NAME} ipp${SILLY_NAME}
-            PATHS /opt
-            PATH_SUFFIXES ${INTEL_PATH}/linux/ipp/lib/intel64_lin
+            PATHS "${IPP_PATH}/lib/intel64_${IPP_PLATFORM_SHORT}"
         )
 
         if (IPP_${PRETTY_NAME})
