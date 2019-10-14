d22392be263cf8a35e8d15eab2f0c7b6546f5724
Chris Kitching <chriskitching@linux.com>
2019-10-15 00:45:16 +0100

    Strip out the problematic per-project documentation flags
    Since each of these per-project flags were initialised (and
    cached) from the global flag, regenerating your build system
    with the global flag inverted would not alter the (independent)
    per-project flags, causing all documentation to remain disabled
    (or enabled).
    
    Practically, the need to generate only some of the documentation
    doesn't really come up. If wanted, it can be achieved using
    `make speclib_docs` or suchlike to build a particular target
    (this is the sort of thing targets traditionally solve, after
    all).
    
    Now the global flag works as expected, we haven't lost the
    ability to generate partial manuals (in the unlikely event we
    ever want to), and significant buildsystem complexity has
    gone away).

--- scripts/XCMake.cmake
+++ scripts/XCMake.cmake
@@ -1,7 +1,3 @@
-# Provide uppercase and lowercase versions of PROJECT_NAME
-string(TOUPPER ${PROJECT_NAME} XCMAKE_PROJECT_NAME_UPPER)
-string(TOLOWER ${PROJECT_NAME} XCMAKE_PROJECT_NAME_LOWER)
-
 # Include the TC _yet again_, because cmake drops some definitions during project() on some platforms,
 # making some things undefined that should be empty-string...
 include("${CMAKE_TOOLCHAIN_FILE}")
@@ -9,4 +5,4 @@ include("${CMAKE_TOOLCHAIN_FILE}")
 ## Include the rest of xcmake, for convenience.
 include(GTest)
 include(Properties)
-include(Flags)
+include(Documentation)
