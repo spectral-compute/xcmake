20922af1155d394a2574baa1150ccf06dbb96190
Chris Kitching <chriskitching@linux.com>
2019-10-17 00:09:44 +0100

    Compute a project GUID, suitable for packaging

--- scripts/Init.cmake
+++ scripts/Init.cmake
@@ -79,6 +79,25 @@ else()
     message(FATAL_ERROR "Unsupported build type: ${CMAKE_BUILD_TYPE}")
 endif()
 
+# Compute the project GUID as a hash of name and build type. We cheatily just truncate the hash and insert hypens to
+# format it as a GUID that's consumed by various tools.
+string(SHA512 XCMAKE_PROJECT_HASH "${CMAKE_PROJECT_NAME}${CMAKE_BUILD_TYPE}")
+string(TOUPPER "${XCMAKE_PROJECT_HASH}" XCMAKE_PROJECT_HASH)
+default_cache_value(XCMAKE_PROJECT_HASH ${XCMAKE_PROJECT_HASH})
+
+# Reformat the hash as `XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX`. Can't use REGEX_REPLACE because it doesn't do variable
+# quantifiers :(
+function (set_project_guid)
+    string(SUBSTRING ${XCMAKE_PROJECT_HASH} 0 8 GUID_0)
+    string(SUBSTRING ${XCMAKE_PROJECT_HASH} 8 4 GUID_1)
+    string(SUBSTRING ${XCMAKE_PROJECT_HASH} 12 4 GUID_2)
+    string(SUBSTRING ${XCMAKE_PROJECT_HASH} 16 4 GUID_3)
+    string(SUBSTRING ${XCMAKE_PROJECT_HASH} 20 12 GUID_4)
+    default_cache_value(XCMAKE_PROJECT_GUID "${GUID_0}-${GUID_1}-${GUID_2}-${GUID_3}-${GUID_4}")
+endfunction()
+
+set_project_guid()
+
 # Target properties to track DLL search paths.
 define_property(TARGET PROPERTY DLL_SEARCH_PATHS
     BRIEF_DOCS "List of paths where dependent DLLs are found"
@@ -116,7 +135,7 @@ include(OnExit)
 include(Summary)
 include(Doxygen)
 include(Pandoc)
-include(CPackStuff)
+include(Packaging)
 
 # All targets should, by default, have hidden visibility. This isn't in the toolchain because it's useful to be able to
 # build others' libraries with that toolchain.
