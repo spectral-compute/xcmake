7573d8c8b5deb4ccb0f62ce53c6df9ee56a92d44
Chris Kitching <chriskitching@linux.com>
2019-10-17 00:36:01 +0100

    Initial CPack config for most platforms

--- VendorConfig.cmake
+++ VendorConfig.cmake
@@ -3,5 +3,14 @@
 # The name of your organisation.
 set(XCMAKE_COMPANY_NAME "Spectral Compute LTD")
 
+# The organisation's website.
+set(XCMAKE_COMPANY_WEBSITE "https://spectralcompute.co.uk")
+
+# A path-friendly veriant of your organisation name.
+set(XCMAKE_COMPANY_PATH_NAME "Spectral Compute")
+
 # A path to your organisation's logo. You can override logos per-project, too.
 set(XCMAKE_COMPANY_LOGO_PATH "${XCMAKE_RESOURCE_DIR}/logo_nowords.svg")
+
+# An email address your organisation can be reached for help.
+set(XCMAKE_COMPANY_HELP_EMAIL "help@spectralcompute.co.uk")
