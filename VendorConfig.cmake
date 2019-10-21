598ea05b1c5d3c81a521b1b72f47d1e6c655ca51
Chris Kitching <chriskitching@linux.com>
2019-10-22 00:47:49 +0100

    Pretty pictures for the installer (Thanks Ben!)

--- VendorConfig.cmake
+++ VendorConfig.cmake
@@ -13,5 +13,8 @@ set(XCMAKE_COMPANY_PATH_NAME "Spectral Compute")
 # png and an svg.
 set(XCMAKE_COMPANY_LOGO_PATH "${XCMAKE_RESOURCE_DIR}/logo_nowords")
 
+# A directory containing `banner.png` and `dialog.png`, used to style the WIX installer (if used).
+set(XCMAKE_WIX_INSTALLER_BRANDING "${XCMAKE_RESOURCE_DIR}/wix_branding")
+
 # An email address your organisation can be reached for help.
 set(XCMAKE_COMPANY_HELP_EMAIL "help@spectralcompute.co.uk")
