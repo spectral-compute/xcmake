e636fa60b3bf494f8d60ea5525c2f448258e5d9f
Chris Kitching <chriskitching@linux.com>
2019-10-22 06:24:57 +0100

    Automatically pick up and apply any WIX patches provided by client applications.

--- scripts/Packaging.cmake
+++ scripts/Packaging.cmake
@@ -62,6 +62,12 @@ default_value(CPACK_WIX_PRODUCT_ICON "${XCMAKE_COMPANY_LOGO_PATH}.png")
 default_value(CPACK_WIX_UI_BANNER "${XCMAKE_WIX_INSTALLER_BRANDING}/banner.png")
 default_value(CPACK_WIX_UI_DIALOG "${XCMAKE_WIX_INSTALLER_BRANDING}/dialog.png")
 
+# If present, apply the project's WIXPatch. This allows manual tinkering with the absurd XML from which the
+# installer is eventually generated.
+if (EXISTS "${CMAKE_SOURCE_DIR}/WIXPatch.xml")
+    default_value(CPACK_WIX_PATCH_FILE "${CMAKE_SOURCE_DIR}/WIXPatch.xml")
+endif()
+
 # Set default generator per-platform
 if (WIN32)
     default_value(CPACK_GENERATOR "WIX")
