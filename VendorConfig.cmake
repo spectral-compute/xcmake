9829ea0b3ae14844e9c61e6d4773833e4041555a
Chris Kitching <chris@spectralcompute.co.uk>
2020-01-17 14:39:27 +0100

    Remove the Spectral specific branding
    Co-authored-by: Nicholas Tomlinson <nstomlinson@gmail.com>

--- VendorConfig.cmake
+++ VendorConfig.cmake
@@ -1,13 +1,13 @@
 # Configruation of who is using xcmake. Used to populate sensible defaults for cpack, logo usage, etc.
 
 # The name of your organisation.
-set(XCMAKE_COMPANY_NAME "Spectral Compute LTD")
+set(XCMAKE_COMPANY_NAME "XCMAKE_COMPANY_NAME")
 
 # The organisation's website.
-set(XCMAKE_COMPANY_WEBSITE "https://spectralcompute.co.uk")
+set(XCMAKE_COMPANY_WEBSITE "XCMAKE_COMPANY_WEBSITE")
 
 # A path-friendly veriant of your organisation name.
-set(XCMAKE_COMPANY_PATH_NAME "Spectral Compute")
+set(XCMAKE_COMPANY_PATH_NAME "XCMAKE_COMPANY_PATH_NAME")
 
 # A path to your organisation's logo. The name should be given without a file extension, and you must provide both a
 # png and an svg.
@@ -17,4 +17,4 @@ set(XCMAKE_COMPANY_LOGO_PATH "${XCMAKE_RESOURCE_DIR}/logo_nowords")
 set(XCMAKE_WIX_INSTALLER_BRANDING "${XCMAKE_RESOURCE_DIR}/wix_branding")
 
 # An email address your organisation can be reached for help.
-set(XCMAKE_COMPANY_HELP_EMAIL "help@spectralcompute.co.uk")
+set(XCMAKE_COMPANY_HELP_EMAIL "XCMAKE_COMPANY_HELP_EMAIL")
