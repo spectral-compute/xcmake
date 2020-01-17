e34cd834224484625a3493b1a8e9d3a1e81ec1fa
Chris Kitching <chriskitching@linux.com>
2020-01-17 10:34:53 +0000

    Enabling debug info in debug builds ain't optional!

--- scripts/properties/DEBUG_INFO.cmake
+++ scripts/properties/DEBUG_INFO.cmake
@@ -11,8 +11,10 @@ define_xcmake_target_property(
     BRIEF_DOCS "Add debug information to the binary"
     DEFAULT ${DEFAULT_DEB}
 )
-target_optional_compile_options(DEBUG_INFO_EFFECTS INTERFACE
+target_compile_options(DEBUG_INFO_EFFECTS INTERFACE
     -g
+)
+target_optional_compile_options(DEBUG_INFO_EFFECTS INTERFACE
     -gcolumn-info
     -fdebug-macro
     -fno-limit-debug-info
