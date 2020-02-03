377a1044cd0751140093aab7b58bad5a2445c421
Chris Kitching <chriskitching@linux.com>
2020-02-03 15:47:41 +0000

    Remove unnecesary explicit XCMAKE_WERROR definition
    Turns out the macro takes care of that :D

--- scripts/properties/WERROR.cmake
+++ scripts/properties/WERROR.cmake
@@ -1,16 +1,9 @@
 include_guard(GLOBAL)
 
-
-if (XCMAKE_WERORR)
-    set(DEFAULT_WERROR "ON")
-else()
-    set(DEFAULT_WERROR "OFF")
-endif()
-
 define_xcmake_target_property(
     WERROR FLAG
     BRIEF_DOCS "Warnings are errors"
-    DEFAULT ${DEFAULT_WERROR}
+    DEFAULT ON
 )
 target_compile_options(WERROR_EFFECTS INTERFACE
     $<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/WX>                      # MSVC
