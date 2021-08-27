a1aa929675ee6b163c98b4d375a27fb8233f0982
Nicholas Tomlinson <nstomlinson@gmail.com>
2021-08-27 20:30:43 +0100

    Make sure our C++ code gets built with CUDA enhanced address spaces

--- scripts/CUDA.cmake
+++ scripts/CUDA.cmake
@@ -146,6 +146,11 @@ function(add_cuda_to_target TARGET)
     else()
         message(FATAL_ERROR "You didn't specify any GPU targets with -DXCMAKE_GPUS, so CUDA targets are not supported.")
     endif()
+
+    # We want non-CUDA C++ code that's part of a CUDA project to use enhanced address spaces, but we don't want to
+    # require that any resulting library can only be used by stuff compiled with our compiler. That's why this isn't
+    # part of the CUDA interface target.
+    target_compile_options("${TARGET}" PRIVATE -fscale-addrspaces)
 endfunction()
 
 # Add an executable that uses CUDA.
