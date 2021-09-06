fd1f4636496ee010ab3a703a87feeb4f8657a1a6
Nicholas Tomlinson <nstomlinson@gmail.com>
2021-09-06 16:22:08 +0100

    Improve portability of generated documentation

--- tools/pandoc/url-rewriter.sh
+++ tools/pandoc/url-rewriter.sh
@@ -2,18 +2,38 @@
 
 INPUT_FILE="$1"
 OUTPUT_FILE="$2"
-DOTSLASHES="$3"
-shift 3
-REPLACEMENTS="$@"
+RELATIVE_TO="$3"
+DOTSLASHES="$4"
+shift 4
 
 # Use sed to turn the input cmake list literal into a sed program.
-# The input is of the form:
+# The inputs are of the form:
 # <URL to match>|<Replacement>
 # <Replacement> is a relative path from the root of the install tree. The idea is that the given
 # URL to match is equivalent to `./<Replacement>` in the install tree.
 # We prepend $DOTSLASHES to `<Replacement>` to eliminate any subdirectories the input .md file
-# might be inside, and then reformat the list as a list of simple replace commands.
-REPLACEMENT_SED=$(echo $REPLACEMENTS | sed -Ee 's#\|#|'$DOTSLASHES'#g;s/ /|g;s|/g;s/^/s|/;s/$/|g/')
+# might be inside, make the path relative to $RELATIVE_TO (so it's maximally portable), and then reformat the list as a
+# list of simple replace commands.
+
+for ORIGINAL_REPLACEMENT in "$@" ; do
+    # The replacement should have the form `OLD|NEW`. Extract the original replacement's components.
+    ORIGINAL_REPLACEMENT_LHS="$(echo "$ORIGINAL_REPLACEMENT" | sed -E 's/([^|]*)[|].*/\1/')"
+    ORIGINAL_REPLACEMENT_RHS="$(echo "$ORIGINAL_REPLACEMENT" | sed -E 's/[^|]*[|](.*)/\1/')"
+
+    # Strip any trailing / off the end of the thing to replace and make the thing to replace with be as relative as
+    # possible (this improves moveability).
+    NEW_REPLACEMENT_LHS="$(echo "$ORIGINAL_REPLACEMENT_LHS" | sed -E 's|/$||')"
+    NEW_REPLACEMENT_RHS="$(realpath -m --relative-to="$RELATIVE_TO" "$ORIGINAL_REPLACEMENT_RHS")"
+
+    # Build the complete sed script for this replacement.
+    NEW_REPLACEMENT="s|${NEW_REPLACEMENT_LHS}|${NEW_REPLACEMENT_RHS}|g"
+
+    # Append the replacement to the complete sed script we're building.
+    if [ ! -z "${REPLACEMENT_SED}" ] ; then
+        REPLACEMENT_SED="${REPLACEMENT_SED};"
+    fi
+    REPLACEMENT_SED="${REPLACEMENT_SED}${NEW_REPLACEMENT}"
+done
 
 # Run said sed program, along with a two default rules:
 # - Replace `README.md` in hyperlinks with `index.html`
