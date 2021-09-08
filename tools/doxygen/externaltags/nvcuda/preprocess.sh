613736df93d327c70ea574a5e2a374db1df5d4f7
Nicholas Tomlinson <nstomlinson@gmail.com>
2021-09-08 01:15:20 +0100

    Include driver_types.h in the nvcuda tag file generation
    This way, we get links to all the enums and stuff.

--- tools/doxygen/externaltags/nvcuda/preprocess.sh
+++ tools/doxygen/externaltags/nvcuda/preprocess.sh
@@ -4,14 +4,20 @@
 
 # CUDA include directory...
 CUDA_INC_DIR="$1"
+shift
 
-cp "$CUDA_INC_DIR/cuda_runtime_api.h" "$2"
+for F in "$@" ; do
+    cp "$CUDA_INC_DIR/$(basename "$F")" "$F"
+done
 
 # Iterate over the CUDA API functions (BECAUSE THIS SEEMS FAST)
-for i in $(cat "$CUDA_INC_DIR/cuda_runtime_api.h" | grep "extern " | grep cuda | sed -Ee 's/.+ ([a-zA-Z0-9]+)\(.+?/\1/'); do
-    # Is this part of the host runtime API?
-    if grep -q $i "$CUDA_INC_DIR/cuda_device_runtime_api.h"; then
-        # Add the __device__ specifier to it.
-        sed -i -Ee 's/__host__ (.*'$i'.*)/__host__ __device__ \1/;s/__device__ __device__/__device__/g' "$2"
-    fi
+for F in "$@" ; do
+    for i in $(cat "$CUDA_INC_DIR/$(basename "$F")" | grep "extern " | grep cuda | sed -Ee 's/.+ ([a-zA-Z0-9]+)\(.+?/\1/'); do
+        # Is this part of the host runtime API?
+        if grep -q $i "$CUDA_INC_DIR/cuda_device_runtime_api.h"; then
+            # Add the __device__ specifier to it.
+            sed -i -Ee 's/__host__ (.*'$i'.*)/__host__ __device__ \1/;s/__device__ __device__/__device__/g' "$F"
+        fi
+    done
+    sed -i -Ee 's/__device_builtin__//g' "$F"
 done
