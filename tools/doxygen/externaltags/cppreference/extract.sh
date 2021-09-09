25984899019d5089738cd89c1476010d72000b13
Nicholas Tomlinson <nstomlinson@gmail.com>
2021-09-09 06:00:57 +0100

    Process the cppreference tag file a little
    Things like std::move need processing a little because there are
    actually two copies (one in <utility> which we normally want, and
    one in <algorithm>). This confuses Doxygen (and the Markdown
    preprocessor).

--- tools/doxygen/externaltags/cppreference/extract.sh
+++ tools/doxygen/externaltags/cppreference/extract.sh
@@ -6,5 +6,15 @@ set -e
 TARBALL="$(realpath "$1")"
 OUT="$(realpath "$2")"
 
-# Extract.
-tar xf "${TARBALL}" -O cppreference-doxygen-web.tag.xml > "${OUT}"
+# Generate a sed script by concatenating the sub-scripts listed in this array.
+REPLACEMENTS=(
+    # There's actually a std::move in <algorithm> too, and this confuses doxygen.
+    's|<name>move \(utility\)</name>|<name>move</name>|'
+)
+for R in "${REPLACEMENTS[@]}" ; do
+    SED_SCRIPT="${SED_SCRIPT}${R};"
+done
+
+# Extract and filter. --force-local is required because Windows treats the `:` in a path as belonging to
+# a remote machine. For example, `C:/`...
+tar xf "${TARBALL}" --force-local -O cppreference-doxygen-web.tag.xml | sed -E "${SED_SCRIPT}" > "${OUT}"
