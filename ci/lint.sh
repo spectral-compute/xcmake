a657972181d5e67490bbd6af01ddea7964b2ea2a
Nicholas Tomlinson <nstomlinson@gmail.com>
2021-09-18 22:41:47 +0100

    Allow nice formating of multi-arg calls
    E.g:
    
    some_function(
        KITTENS "some fairly long string 1"
                "some farirly long string 2"
                "Kittens become cats"
        CATS "Even cats like strings"
             "This is neater than if we banned more than 4 spaces"
        NO_DOGS
    )

--- ci/lint.sh
+++ ci/lint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cmakelint --filter=-whitespace/extra,-linelength,-convention/filename,-package/stdargs --spaces=4 \
+cmakelint --filter=-whitespace/extra,-whitespace/indent,-linelength,-convention/filename,-package/stdargs --spaces=4 \
     $(find $1 -name \*.cmake -o -name CMakeLists.txt)
