1990289b3f7fbbbf5ef75447a4485f55ab6b7128
Chris Kitching <chriskitching@linux.com>
2021-10-05 22:58:35 +0100

    MAke clion's language service work

--- scripts/Targets.cmake
+++ scripts/Targets.cmake
@@ -301,12 +301,18 @@ function(apply_default_standard_properties TARGET)
             -fdiagnostics-show-template-tree
             -fdiagnostics-show-option
             -fdiagnostics-show-category=name
-
-            # Emit an error if we accidentally code-gen jumbo-sized objects (even if these would be removed by optimization,
-            # it's better not to generate them in the first place).
-            -fmax-data-global-size=67108864
-            -fmax-data-local-size=1048576
         )
+
+
+        if (NOT DEFINED ENV{CLION_IDE})
+            # Some flags break clion's clangd, so need to be omitted.
+            target_compile_options(${TARGET} BEFORE PRIVATE
+                # Emit an error if we accidentally code-gen jumbo-sized objects (even if these would be removed by optimization,
+                # it's better not to generate them in the first place).
+                -fmax-data-global-size=67108864
+                -fmax-data-local-size=1048576
+            )
+        endif()
     endif()
 
 
