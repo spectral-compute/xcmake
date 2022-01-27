5ce438973f98c2845ec061f49894cdd4742ed6a9
Chris Kitching <chriskitching@linux.com>
2022-01-27 19:28:47 +0000

    Trademark sanitiser now simply bans the symbols on the listed words
    We can still, by omitting a word, use the symbols with any trademarks
    we end up owning, licensing, or whatever.

--- tools/tm-sanitiser.sh
+++ tools/tm-sanitiser.sh
@@ -3,8 +3,7 @@
 # A script to sanitise files for trademark compliance.
 
 # Applies the following rules to the input file:
-# - The first use of the mark must be immediately followed by the symbol.
-# - The first use of the mark must be immediately preceded by the owner.
+# - The (r) or ^{tm} symbols cannot appear following any of the trademarks in the input configruation.
 #
 # A descriptive error is printed and the script returns nonzero if a problem is found.
 
@@ -22,32 +21,15 @@ for TM in "$@"; do
     # Who owns the trademark?
     OWNER=$(echo "$TM" | cut -d ' ' -f 1)
 
-    # The symbol identifying the type of mark
-    SYMBOL=$(echo "$TM" | sed -Ee 's/.*([™®]).*/\1/g')
-
     # The mark itself.
     MARK=$(echo "$TM" | cut -d ' ' -f 2- | sed -Ee 's/[™®]//g')
 
-    FIRST=$(cat "$TARGET" | grep -n "$MARK" | head -n 1)
-
-    # If the trademark isn't found, skip it.
-    if [ "$FIRST" == "" ]; then
-        continue;
-    fi
-
-    LINE=$(echo $FIRST | cut -d ':' -f 1)
+    FINDING=$(cat "$TARGET" | grep -n "$MARK[™®]" | head -n 1)
+    LINE=$(echo $FINDING | cut -d ':' -f 1)
 
     # Has the symbol?
-    if ! echo "$FIRST" | grep -q "$MARK$SYMBOL"; then
-        explode "$TARGET" $LINE "$MARK" "Missing $SYMBOL for first use of trademark \"$MARK\""
+    if [ ! -z "$FINDING" ]; then
+        explode "$TARGET" $LINE "$MARK" "Inappropriate symbol for use of trademark \"$MARK\""
         exit 1
-    fi
-
-    # Has the owner name?
-    if [ "$OWNER" != "" ]; then
-        if ! echo "$FIRST" | grep -Eq "$OWNER[™®]? $MARK"; then
-            explode "$TARGET" $LINE "$MARK" "Missing trademark attribution (\"$OWNER\") for first use of \"$MARK\""
-            exit 1
-        fi
     fi
 done
