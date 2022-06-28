d797b61f2c30d912c7be0d55a10b2fc9957c0b9b
Nicholas Tomlinson <nstomlinson@gmail.com>
2022-06-28 10:53:16 +0100

    Add cross-compilation test

---
+++ test/cross/test.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+set -e
+
+# Arguments that are common between test scripts.
+XCMAKE_DIR="$1"
+TMP_DIR="$2"
+shift 2
+
+# Default arguments.
+CMAKE_ARGS=()
+
+# Get arguments out of the environment.
+while [ "$#" != "0" ] ; do
+    ARG="$1"
+    shift
+    case "${ARG}" in
+        -c|--toolchain-base-dir)
+            echo ">>>> XCMAKE_TOOLCHAIN_BASE_DIR"
+            CMAKE_ARGS+=(-DXCMAKE_TOOLCHAIN_BASE_DIR="$1")
+            shift
+        ;;
+    esac
+done
+
+# Test function.
+function testTribble {
+    TRIBBLE="$1"
+    shift
+
+    echo -e "\e[33;1mTesting tribble\e[m: \e[1m${TRIBBLE}\e[m"
+
+    echo -e "\e[34;1mTribble Info\e[m"
+    cmake -DXCMAKE_TRIBBLE=${TRIBBLE} -DXCMAKE_SHOW_TRIBBLE=On "${CMAKE_ARGS[@]}" \
+          -P "${XCMAKE_DIR}/toolchain/toolchain.cmake"
+
+    echo -e "\e[34;1mBuild simple program\e[m"
+
+    # Change to the build directory for this tribble's test.
+    mkdir -p "${TMP_DIR}/${TRIBBLE}"
+    cd "${TMP_DIR}/${TRIBBLE}"
+
+    # Allow errors so we can report them.
+    set +e
+
+    # Configure.
+    cmake \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="install" \
+        -DCMAKE_PREFIX_PATH="${XCMAKE_DIR}/../" \
+        -DXCMAKE_TRIBBLE=${TRIBBLE} \
+        "${CMAKE_ARGS[@]}" \
+        "${XCMAKE_DIR}/test/common/simple"
+    if [ "$?" != "0" ] ; then
+        echo -e "\e[31;1mError\e[m: cmake configure failed" 1>&2
+        return 1
+    fi
+
+    # Run make.
+    MAKE_OUTPUT="$(make install VERBOSE=1 2>&1)"
+    E="$?"
+    echo "${MAKE_OUTPUT}"
+
+    if [ "$E" != "0" ] ; then
+        echo -e "\e[31;1mError\e[m: make failed" 1>&2
+        return 1
+    fi
+
+    # No more errors.
+    set -e
+
+    # Check the output for the required patterns.
+    while [ "$#" != "0" ] ; do
+        if ! echo "${MAKE_OUTPUT}" | grep -qE "$1" ; then
+            echo -e "\e[31;1mError\e[m: Compiler output did not match $1" 1>&2
+            return 1
+        fi
+        shift
+    done
+
+    # We're done with this directory.
+    cd -
+}
+
+# Test some tribbles.
+testTribble "native" \
+            "clang[+][+] .*-march=native" \
+            "clang[+][+] .*-mtune=native"
+testTribble "ubuntu16.04-aarch64-generic" \
+            "clang[+][+] .*--target=aarch64-unknown-linux-gnu" \
+            "clang[+][+] .*-mcpu=generic"
