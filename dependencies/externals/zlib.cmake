02163b3f8f539319665fa076e779f19a66760010
Chris Kitching <chriskitching@linux.com>
2022-07-27 02:22:42 +0100

    Add libzip and zlib external build scripts

---
+++ dependencies/externals/zlib.cmake
@@ -0,0 +1,18 @@
+include_guard(GLOBAL)
+include(ExternalProj)
+
+# On Windows, we need _CRT_DECLARE_NONSTDC_NAMES.
+if (WIN32)
+    set(COMPILE_OPTIONS ${COMPILE_OPTIONS} PUBLIC -D_CRT_DECLARE_NONSTDC_NAMES)
+endif()
+
+add_external_project(zlib_proj
+    GIT_REPOSITORY    https://github.com/madler/zlib.git
+    GIT_TAG           v1.2.12
+    CMAKE_ARGS
+        "-DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} ${COMPILE_OPTIONS}"
+        "-DCMAKE_C_FLAGS=${CMAKE_CXX_FLAGS} ${COMPILE_OPTIONS}"
+        "-DCMAKE_BUILD_TYPE=Release"
+    STATIC_LIBRARIES
+        z
+)
