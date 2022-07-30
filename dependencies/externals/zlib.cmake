a683b3ae34507fbc45d240b61a53763ef999a97d
Nicholas Tomlinson <nstomlinson@gmail.com>
2022-07-30 03:37:28 +0100

    Don't link against the zlib DLL on Windows

--- dependencies/externals/zlib.cmake
+++ dependencies/externals/zlib.cmake
@@ -4,14 +4,15 @@ include(ExternalProj)
 # On Windows, we need _CRT_DECLARE_NONSTDC_NAMES.
 set(LIB_NAME z)
 if (WIN32)
-    set(LIB_NAME zlib)
+    set(LIB_NAME zlibstatic)
     set(COMPILE_OPTIONS -D_CRT_DECLARE_NONSTDC_NAMES)
 endif()
 
 add_external_project(zlib_proj
     GIT_REPOSITORY    https://github.com/madler/zlib.git
     GIT_TAG           v1.2.12
-    CMAKE
+    CMAKE_ARGS
+        -DBUILD_SHARED_LIBS=OFF
     C_FLAGS "${COMPILE_OPTIONS}"
     STATIC_LIBRARIES
         ${LIB_NAME}
