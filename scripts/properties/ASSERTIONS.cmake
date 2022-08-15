d37c3d4dfa2b454e90f234dc957c87867ed36903
Chris Kitching <chriskitching@linux.com>
2022-08-15 23:30:59 +0100

    Finish off libc++ support

--- scripts/properties/ASSERTIONS.cmake
+++ scripts/properties/ASSERTIONS.cmake
@@ -16,6 +16,9 @@ target_compile_definitions(ASSERTIONS_EFFECTS INTERFACE
 
     # Enable assertions baked into the gnu STL.
     -D_GLIBCXX_DEBUG
+
+    # Enable libc++ assertions, too.
+    -D_LIBCPP_ENABLE_ASSERTIONS
 )
 target_optional_compile_options(ASSERTIONS_EFFECTS INTERFACE
     # Don't warn about optimiser failures (assertions cause lots of them).
