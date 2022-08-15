03ae97fb64f7c8d2733ec06d4c374f87b0907f60
Chris Kitching <chriskitching@linux.com>
2022-08-15 23:36:54 +0100

    Don't combine C++ and C flags for EPs

--- scripts/ExternalProj.cmake
+++ scripts/ExternalProj.cmake
@@ -107,7 +107,7 @@ function(add_external_project TARGET)
     set(CMAKE_ARGS "")
     if(ep_CMAKE OR ep_CMAKE_ARGS)
         string(JOIN " " CXX_FLAGS ${XCMAKE_EP_CXX_FLAGS} ${ep_CXX_FLAGS})
-        string(JOIN " " C_FLAGS ${XCMAKE_EP_CXX_FLAGS} ${ep_C_FLAGS})
+        string(JOIN " " C_FLAGS ${XCMAKE_EP_C_FLAGS} ${ep_C_FLAGS})
         string(JOIN " " LINKER_FLAGS ${XCMAKE_EP_LINKER_FLAGS})
 
         list(APPEND CMAKE_ARGS
