360da857fdb60b418f73f6228f1089a9dc0d9f2f
Nicholas Tomlinson <nstomlinson@gmail.com>
2022-08-30 00:27:33 +0100

    Add pcpp support to add_headers

---
+++ test/add_headers/pcpp_compressed/ref/a.h
@@ -0,0 +1,15 @@
+// A comment for header A.
+// Annoyingly, pcpp removes #pragma once.
+void fn();
+#define CAT
+#define KITTEN
+void happy_cat();
+void singLifeFormsSong();
+#ifdef DEATH
+void sad();
+#else // DEATH
+void ok();
+#endif // DEATH
+void what();
+#define MEANING LIFE // This is annoying.
+void life();
---
+++ test/add_headers/pcpp_compressed/ref/b.h
@@ -0,0 +1,18 @@
+#ifndef B_H
+#define B_H
+/* A comment for header B. */
+#ifdef __cplusplus
+namespace
+{
+#endif // __cplusplus
+/**
+ * Get the meaning of life as an integer.
+ */
+int life()
+{
+    return 42;
+}
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+#endif // B_H
