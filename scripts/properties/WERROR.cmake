94a4ad78450ad17bdbbd76fa9bdff98c6ecca131
Chris Kitching <chriskitching@linux.com>
2023-05-04 04:34:46 +0100

    No werror

--- scripts/properties/WERROR.cmake
+++ scripts/properties/WERROR.cmake
@@ -3,7 +3,7 @@ include_guard(GLOBAL)
 define_xcmake_target_property(
     WERROR FLAG
     BRIEF_DOCS "Warnings are errors"
-    DEFAULT ON
+    DEFAULT OFF
 )
 target_compile_options(WERROR_EFFECTS INTERFACE
     $<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/WX>                      # MSVC
