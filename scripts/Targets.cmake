cd53dfeaaae9aaeb32f4e79fd75568af63027855
Nicholas Tomlinson <nstomlinson@gmail.com>
2023-05-29 15:04:26 +0100

    Allow lambda captures to shadow the outer scope
    This is useful for things like: `[var = std::move(var)](){}`.

--- scripts/Targets.cmake
+++ scripts/Targets.cmake
@@ -254,6 +254,7 @@ function(apply_default_standard_properties TARGET)
             -Wno-shadow-field
             -Wno-shadow-field-in-constructor     # Sorry Nick, I like doing this. :D
             -Wshadow-field-in-constructor-modified # The above turns this off, but we want it back on.
+            -Wno-shadow-uncaptured-local         # A useful pattern is to std::move something into a lambda.
             -Wno-global-constructors             # We also use these
             -Wno-missing-prototypes
             -Wno-switch-enum                     # This is stupid.
