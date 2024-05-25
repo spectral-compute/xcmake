b1d4155b42a7a009c9cf98d8119987375928aa1b
Nicholas Tomlinson <nstomlinson@gmail.com>
2024-05-25 21:36:26 +0100

    Add XCMAKE_SYSTEM_DOCOPT
    I prefer it. It defaults to off. Everyone's happy :)

--- dependencies/externals/docopt-cpp.cmake
+++ dependencies/externals/docopt-cpp.cmake
@@ -1,15 +1,22 @@
 include_guard(GLOBAL)
-include(ExternalProj)
 
-set(DOCOPT_FLAGS "")
-if(MSVC)
-    set(DOCOPT_FLAGS "/EHsc") # Sets the exception handling mode
+option(XCMAKE_SYSTEM_DOCOPT "Use system docopt.cpp rather than build our own" Off)
+
+if (XCMAKE_SYSTEM_DOCOPT)
+    find_package(docopt REQUIRED)
+else()
+    include(ExternalProj)
+
+    set(DOCOPT_FLAGS "")
+    if(MSVC)
+        set(DOCOPT_FLAGS "/EHsc") # Sets the exception handling mode
+    endif()
+
+    add_external_project(docopt_proj
+        GIT_REPOSITORY https://github.com/docopt/docopt.cpp.git
+        GIT_TAG 42ebcec9dc2c99a1b3a4542787572045763ad196
+        CMAKE
+        CXX_FLAGS "${DOCOPT_FLAGS}"
+        STATIC_LIBRARIES docopt
+    )
 endif()
-
-add_external_project(docopt_proj
-    GIT_REPOSITORY https://github.com/docopt/docopt.cpp.git
-    GIT_TAG 42ebcec9dc2c99a1b3a4542787572045763ad196
-    CMAKE
-    CXX_FLAGS "${DOCOPT_FLAGS}"
-    STATIC_LIBRARIES docopt
-)
