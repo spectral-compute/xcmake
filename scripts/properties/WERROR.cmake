fed000eb3331167377e803012a04920d9b07dfc9
SollyCB <solomoncardenbrown@gmail.com>
2025-02-06 18:44:31 +0000

    Use appropriate Werror when compiling with NVCC

--- scripts/properties/WERROR.cmake
+++ scripts/properties/WERROR.cmake
@@ -6,6 +6,7 @@ define_xcmake_target_property(
     DEFAULT ON
 )
 target_compile_options(WERROR_EFFECTS INTERFACE
-    $<$<CXX_COMPILER_ID:MSVC>:/WX>                          # MSVC
-    $<$<CXX_COMPILER_ID:Clang,AppleClang,GNU>:-Werror>      # Clang/GCC
+    $<$<CXX_COMPILER_ID:MSVC>:/WX>                     # MSVC
+    $<$<CXX_COMPILER_ID:Clang,AppleClang,GNU>:-Werror> # Clang/GCC
+    $<$<COMPILE_LANGUAGE:CUDA>:-Werror all-warnings>   # NVCC
 )
