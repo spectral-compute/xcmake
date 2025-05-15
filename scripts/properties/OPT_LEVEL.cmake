e1a7f8c56782cbdde63cfc357394e3288a022b68
Chris Kitching <chriskitching@linux.com>
2025-05-15 14:11:42 +0200

    Remove fast-math from the xcmake OPT_LEVEL stuff
    With the retirement of `-Ofast`, and the introduction of far
    more varied fast-math flags than once existed, this probably
    needs to be handled specifically by projects.
    
    A notable pitfall of the old approach was that enabling
    fast-math flags on some TUs but not others would not work
    as desired, since some of the new fast-math stuff is actually
    based on link-time stuff, so affects an entire library/executable
    regardless.

--- scripts/properties/OPT_LEVEL.cmake
+++ scripts/properties/OPT_LEVEL.cmake
@@ -1,15 +1,15 @@
 include_guard(GLOBAL)
 
-default_value(XCMAKE_OPT_LEVEL_Debug none)
-default_value(XCMAKE_OPT_LEVEL_Release unsafe)
-default_value(XCMAKE_OPT_LEVEL_RelWithDebInfo unsafe)
+default_value(XCMAKE_OPT_LEVEL_Debug debug)
+default_value(XCMAKE_OPT_LEVEL_Release speed)
+default_value(XCMAKE_OPT_LEVEL_RelWithDebInfo speed)
 
 define_xcmake_target_property(
     OPT_LEVEL
     BRIEF_DOCS "Optimisation level to use"
-    FULL_DOCS "Valid values are: none, debug, size, safe, and unsafe. Does more than just setting -Ofoo!"
+    FULL_DOCS "Valid values are: none, debug, speed, size. `debug` is for step-through debugging, `none` should probably not be used unless you have a very specific reason, and `speed` is a normal optimised build (the default)"
     DEFAULT ${XCMAKE_OPT_LEVEL_${CMAKE_BUILD_TYPE}}
-    VALID_VALUES none debug size safe unsafe
+    VALID_VALUES none debug speed size
 )
 
 # This one has to be function-style so we can run a generator expression on TARGET.
@@ -22,23 +22,22 @@ function(OPT_LEVEL_EFFECTS TARGET)
     # Individual optimization levels.
     add_library(${TARGET}_none_OPT_LEVEL_EFFECTS INTERFACE)
     add_library(${TARGET}_debug_OPT_LEVEL_EFFECTS INTERFACE)
-    add_library(${TARGET}_safe_OPT_LEVEL_EFFECTS INTERFACE)
+    add_library(${TARGET}_speed_OPT_LEVEL_EFFECTS INTERFACE)
     add_library(${TARGET}_size_OPT_LEVEL_EFFECTS INTERFACE)
-    add_library(${TARGET}_unsafe_OPT_LEVEL_EFFECTS INTERFACE)
 
     target_compile_options(${TARGET}_none_OPT_LEVEL_EFFECTS INTERFACE
-            $<$<COMPILE_LANG_AND_ID:CUDA,NVIDIA>:-O0>                   # NVCC
-            $<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/Od>                      # MSVC
-            $<${IS_CLANG}:-O0>
-            ${CLANG_LTO_FLAGS}
+        $<$<COMPILE_LANG_AND_ID:CUDA,NVIDIA>:-O0>                   # NVCC
+        $<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/Od>                      # MSVC
+        $<${IS_CLANG}:-O0>
+        ${CLANG_LTO_FLAGS}
     )
     target_optional_compile_options(${TARGET}_none_OPT_LEVEL_EFFECTS INTERFACE
         -Wno-pass-failed  # Don't complain that loops didn't unroll and so on just because the pass is not enabled.
     )
 
     target_compile_options(${TARGET}_size_OPT_LEVEL_EFFECTS INTERFACE
-        $<$<COMPILE_LANG_AND_ID:CUDA,NVIDIA>:-O2>                   # NVCC
-        $<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/O1>                      # O1 is "optimise for size" on MSVC...
+        $<$<COMPILE_LANG_AND_ID:CUDA,NVIDIA>:-O2>         # NVCC
+        $<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/O1>            # O1 is "optimise for size" on MSVC...
         $<${IS_CLANG}:-Oz>
         ${CLANG_LTO_FLAGS}
         # Can probably do more here if you care enough to bother...
@@ -53,25 +52,15 @@ function(OPT_LEVEL_EFFECTS TARGET)
         -Wno-pass-failed  # Don't complain that loops didn't unroll and so on just because the pass is not enabled.
     )
 
-    target_compile_options(${TARGET}_safe_OPT_LEVEL_EFFECTS INTERFACE
+    target_compile_options(${TARGET}_speed_OPT_LEVEL_EFFECTS INTERFACE
         $<$<COMPILE_LANG_AND_ID:CUDA,NVIDIA>:-O2>                   # NVCC
         $<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/O2 /Ob2>                 # MSVC
         $<${IS_CLANG}:-O3>
         ${CLANG_LTO_FLAGS}
     )
-    target_optional_compile_options(${TARGET}_safe_OPT_LEVEL_EFFECTS INTERFACE
+    target_optional_compile_options(${TARGET}_speed_OPT_LEVEL_EFFECTS INTERFACE
         -finline-functions
-    )
-
-    target_compile_options(${TARGET}_unsafe_OPT_LEVEL_EFFECTS INTERFACE
-        # NVCC does nothing, since O2 is the default, and it doesn't accept it twice!
-        $<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/O2 /Ob2>
-        $<${IS_CLANG}:-O3 -ffast-math>
-        ${CLANG_LTO_FLAGS}
-    )
-    target_optional_compile_options(${TARGET}_unsafe_OPT_LEVEL_EFFECTS INTERFACE
-        -fstrict-vtable-pointers # An experimental but years-old optimisation.
-        -finline-functions
+        -fstrict-vtable-pointers
     )
 
     # In optimising builds, have the linker delete unused sections.
@@ -79,12 +68,11 @@ function(OPT_LEVEL_EFFECTS TARGET)
     # want `-ffunction-sections`, since it hurts performance and LTO does a much better job anyway.
     # This causes an error on the macOS build server
     if (NOT APPLE)
-        target_link_options(${TARGET}_safe_OPT_LEVEL_EFFECTS INTERFACE "LINKER:--gc-sections")
-        target_link_options(${TARGET}_unsafe_OPT_LEVEL_EFFECTS INTERFACE "LINKER:--gc-sections")
+        target_link_options(${TARGET}_speed_OPT_LEVEL_EFFECTS INTERFACE "LINKER:--gc-sections")
     endif()
 
     # When LTO is enabled, copy the compiler arguments to the linker.
-    foreach (LEVEL IN ITEMS none debug size safe unsafe)
+    foreach (LEVEL IN ITEMS none debug speed size)
         target_link_options(${TARGET}_${LEVEL}_OPT_LEVEL_EFFECTS INTERFACE
                             $<TARGET_PROPERTY:${TARGET}_${LEVEL}_OPT_LEVEL_EFFECTS,INTERFACE_COMPILE_OPTIONS>)
     endforeach()
