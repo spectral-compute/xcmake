c4da74a5edc74f1c03e4eee2b45f93e35b855248
Chris Kitching <chriskitching@linux.com>
2025-05-27 14:52:16 +0200

    Forbid NOLINT without specifying a diagnostic name

--- tools/clang-tidy/clang-tidy.sh
+++ tools/clang-tidy/clang-tidy.sh
@@ -10,7 +10,7 @@ while echo "$1" | grep -qE '^--extra-arg-before[= ]' ; do
 done
 
 printError() {
-    echo -e "\e[1m\e[97m$1: \e[31merror: \e[97m$2\e[0m"
+    echo -e "$1:\e[31m error\e[0m: $2"
 }
 
 # Function for detecting some kinds of trivial formatting fail.
@@ -19,7 +19,7 @@ findFail() {
     FILE=$2
     shift 2
 
-    FAIL=`grep -Pn $REGEX $FILE | head -n 1 | cut -d ':' -f 1`
+    FAIL=`grep -Pn "$REGEX" $FILE | head -n 1 | cut -d ':' -f 1`
     if [ ! $FAIL = "" ]; then
         printError $FILE:$FAIL "$@"
         exit 1
@@ -30,5 +30,7 @@ findFail() {
 findFail '\t' $1 "line indented with tab characters"
 findFail $'\\s$' $1 "Trailing whitespace"
 findFail $'\r'\$ $1 "CRLF detected - use LF instead"
+findFail $'NOLINT$' $1 "NOLINT given without diagnostic name"
+findFail $'NOLINT[ ]' $1 "NOLINT given without diagnostic name"
 
 clang-tidy ${EXTRA_ARGS} --use-color --header-filter="$SRCDIR/.*" --vfsoverlay="$(dirname "$0")/vfs.yaml" "$@"
