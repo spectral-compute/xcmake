68f6ac1445b108d7b9995d0c06cc5d9d65c40a4f
SollyCB <solomoncardenbrown@gmail.com>
2025-08-22 18:17:24 +0000

    Fix OPT_LEVEL safe
    The interface library was not being created, so linking against it
    caused cmake to emit `-ltarget_safe_OPT_LEVEL` to the commandline.

--- scripts/properties/OPT_LEVEL.cmake
+++ scripts/properties/OPT_LEVEL.cmake
@@ -24,6 +24,7 @@ function(OPT_LEVEL_EFFECTS TARGET)
     add_library(${TARGET}_debug_OPT_LEVEL_EFFECTS INTERFACE)
     add_library(${TARGET}_speed_OPT_LEVEL_EFFECTS INTERFACE)
     add_library(${TARGET}_size_OPT_LEVEL_EFFECTS INTERFACE)
+    add_library(${TARGET}_safe_OPT_LEVEL_EFFECTS INTERFACE)
 
     target_compile_options(${TARGET}_none_OPT_LEVEL_EFFECTS INTERFACE
         $<$<COMPILE_LANG_AND_ID:CUDA,NVIDIA>:-O0>                   # NVCC
@@ -78,8 +79,6 @@ function(OPT_LEVEL_EFFECTS TARGET)
     endforeach()
 
     # I realise this is ridiculous.
-    target_link_libraries(
-        ${TARGET} PRIVATE
-        $<$<BOOL:$<TARGET_PROPERTY:${TARGET},OPT_LEVEL>>:${TARGET}_$<TARGET_PROPERTY:${TARGET},OPT_LEVEL>_OPT_LEVEL_EFFECTS>
-    )
+    set(TOL $<TARGET_PROPERTY:${TARGET},OPT_LEVEL>)
+    target_link_libraries(${TARGET} PRIVATE $<$<BOOL:${TOL}>:${TARGET}_${TOL}_OPT_LEVEL_EFFECTS>)
 endfunction()
