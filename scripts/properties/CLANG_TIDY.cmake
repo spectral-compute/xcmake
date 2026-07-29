c8bc7f50603d6970364973765be870103041dc70
SollyCB <solomon@spectralcompute.co.uk>
2026-07-29 13:59:35 +0000

    Fix clang-tidy path hints
    Plus add more consistent/better quoting.
    
    Lol. Seems I was originally correct to not have quotes here and adding
    them broke it.
    
    Using quotes here, cmake sees a string, as opposed to a list. If the
    `HINTS` variable is empty, this does not break things. The quotes was
    extra defensive against this issue: they were expected to at least
    reduce programmer confusion asking "wait, doesn't this break if empty".
    But they are in fact just harmful.
    
    This is also robust to paths with spaces.

--- scripts/properties/CLANG_TIDY.cmake
+++ scripts/properties/CLANG_TIDY.cmake
@@ -12,21 +12,23 @@ add_custom_target(xcmake_clang_tidy DEPENDS "${XCMAKE_TOOLS_DIR}/clang-tidy/clan
 
 function(find_clang_tidy)
     if (${CMAKE_C_COMPILER_ID} STREQUAL Clang)
-        get_filename_component(DIR ${CMAKE_C_COMPILER} DIRECTORY)
-        list(APPEND HINTS ${DIR})
+        get_filename_component(DIR "${CMAKE_C_COMPILER}" DIRECTORY)
+        list(APPEND HINTS "${DIR}")
     endif()
     if (${CMAKE_CXX_COMPILER_ID} STREQUAL Clang)
-        get_filename_component(DIR ${CMAKE_CXX_COMPILER} DIRECTORY)
-        list(APPEND HINTS ${DIR})
+        get_filename_component(DIR "${CMAKE_CXX_COMPILER}" DIRECTORY)
+        list(APPEND HINTS "${DIR}")
     endif()
 
-    find_program(CLANG_TIDY_BIN clang-tidy HINTS "${HINTS}" REQUIRED)
+    # The `HINTS` variable should not be quoted. The below is robust to paths
+    # with spaces, and the variable being empty.
+    find_program(CLANG_TIDY_BIN clang-tidy HINTS ${HINTS} REQUIRED)
 endfunction()
 
 function(CLANG_TIDY_EFFECTS TARGET)
     if (NOT DEFINED CLANG_TIDY_BIN)
         find_clang_tidy()
     endif()
-    set_target_properties(${TARGET} PROPERTIES CXX_CLANG_TIDY "${XCMAKE_TOOLS_DIR}/clang-tidy/clang-tidy.sh;${CLANG_TIDY_BIN};${CMAKE_SOURCE_DIR}")
+    set_target_properties("${TARGET}" PROPERTIES CXX_CLANG_TIDY "${XCMAKE_TOOLS_DIR}/clang-tidy/clang-tidy.sh;${CLANG_TIDY_BIN};${CMAKE_SOURCE_DIR}")
     add_dependencies("${TARGET}" xcmake_clang_tidy)
 endfunction()
